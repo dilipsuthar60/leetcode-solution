689cefab76f6d49bc4f81cd43d009cf3150b5e1d
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-02-03 20:47:54 +0530

    Time: 234 ms (38.94%), Space: 79.4 MB (39.26%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,21 +1,76 @@
 class Solution
 {
 public:
+    struct node
+    {
+        node*child[26]={NULL};
+        int end=0;
+    };
+    node*root=new node();
+    void insert(string &s)
+    {
+        int n=s.size();
+        node*curr=root;
+        for(int i=0;i<n;i++)
+        {
+            int index=s[i]-'a';
+            if(curr->child[index]==NULL)
+            {
+                curr->child[index]=new node();
+            }
+            curr=curr->child[index];
+        }
+        curr->end=1;
+    }
+    void dfs(node*curr,string &s,vector<string>&res)
+    {
+        if(res.size()==3)
+        {
+            return ;
+        }
+        if(curr->end)
+        {
+            res.push_back(s);
+            
+        }
+        for(char ch='a';ch<='z';ch++)
+        {
+            if(curr->child[ch-'a']!=NULL)
+            {
+                s.push_back(ch);
+                dfs(curr->child[ch-'a'],s,res);
+                s.pop_back();
+            }
+        }
+    }
+    void find(string prefix,vector<string>&res)
+    {
+        node*curr=root;
+        for(int i=0;i<prefix.size();i++)
+        {
+            int index=prefix[i]-'a';
+            if(curr->child[index]==NULL)
+            {
+                return ;
+            }
+            curr=curr->child[index];
+        }
+        dfs(curr,prefix,res);
+    }
     vector<vector<string>> suggestedProducts(vector<string>&nums, string s)
     {
         int n=s.size();
-        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            insert(nums[i]);
+        }
+        string prefix="";
         vector<vector<string>>ans;
-        string now="";
         for(int i=0;i<n;i++)
         {
-            now.push_back(s[i]);
-            int index=lower_bound(nums.begin(),nums.end(),now)-nums.begin();
+            prefix.push_back(s[i]);
             vector<string>res;
-            for(int j=index;j<min(index+3,(int)nums.size())&&nums[j].substr(0,now.size())==now;j++)
-            {
-                res.push_back(nums[j]);
-            }
+            find(prefix,res);
             ans.push_back(res);
         }
         return ans;
