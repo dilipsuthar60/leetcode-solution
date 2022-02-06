bd56b8a0da7737275c69b7bae7e076369fcc8562
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-02-06 10:11:22 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

---
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    long long smallestNumber(long long num) 
+    {
+        if(num==0)
+        {
+            return 0;
+        }
+        int zero=0;
+        int sh=1;
+        string s=to_string(num);
+        for(int i=0;i<s.size();i++)
+        {
+            zero+=(s[i]=='0');
+        }
+        if(s[0]=='-')
+        {
+            sh=-1;
+            s=s.substr(1);
+            sort(s.begin(),s.end(),greater<int>());
+            return -1*stoll(s);    
+        }
+        else
+        {
+            sort(s.begin(),s.end());
+            string str="";
+            if(zero==0)
+            {
+                return stoll(s);
+            }
+            for(int i=0;i<s.size();i++)
+            {
+                if(s[i]!='0')
+                {
+                    str+=s[i];
+                }
+            }
+            string first=str.substr(0,1)+string(zero,'0')+str.substr(1);
+            return stoll(first);
+        }
+        return 1;
+    }
+};
