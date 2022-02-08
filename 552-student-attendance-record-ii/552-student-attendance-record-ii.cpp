4c16d82ef96b2e8be7dd9bb71e943261a73bf6eb
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-02-08 09:30:03 +0530

    Time: 531 ms (19.61%), Space: 21.5 MB (40.00%) - LeetHub

---
+++ 552-student-attendance-record-ii/552-student-attendance-record-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    const int mod=1e9+7;
+    long long dp[100005][3][3];
+    int find(int i,int A,int L,int n)
+    {
+        if(i==n)
+        {
+            return 1;
+        }
+        if(dp[i][A][L]!=-1)
+        {
+            return dp[i][A][L];
+        }
+        int res=0;
+        res=find(i+1,A,0,n);
+        if(A==0)
+        {
+            res=(res+find(i+1,1,0,n))%mod;
+        }
+        if(L<2)
+        {
+            res=(res+find(i+1,A,L+1,n))%mod;
+        }
+        return dp[i][A][L]=res%mod;
+    }
+    int checkRecord(int n) {
+        memset(dp,-1,sizeof(dp));
+        return find(0,0,0,n);
+    }
+};
