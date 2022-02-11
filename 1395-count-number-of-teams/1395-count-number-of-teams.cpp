c1154a5c137a28cdcb37d35332b81f90d44df735
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-02-11 09:58:51 +0530

    Time: 35 ms (97.37%), Space: 74 MB (5.09%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,36 +1,48 @@
 class Solution {
 public:
+    const int N=1e5+5;
+    vector<int>bitl,bitr;
+    void update(int i,int val,vector<int>&bit)
+    {
+        while(i<N)
+        {
+            bit[i]+=val;
+            i+=(i&-i);
+        }
+    }
+    int find(int i,vector<int>&bit)
+    {
+        int sum=0;
+        while(i>0)
+        {
+            sum+=bit[i];
+            i-=(i&-i);
+        }
+        return sum;
+    }
+    int find_(int i,vector<int>&bit)
+    {
+        return find(N-2,bit)-find(i-1,bit);
+    }
     int numTeams(vector<int>& nums) 
     {
+        bitl.resize(N);
+        bitr.resize(N);
         int n=nums.size();
-        int count=0;
-        for(int i=1;i<n-1;i++)
+        for(int i=0;i<n;i++)
         {
-            int left_small,left_big,right_small,right_big;
-            left_small=left_big=right_small=right_big=0;
-            for(int j=0;j<i;j++)
-            {
-                if(nums[j]<nums[i])
-                {
-                    left_small++;
-                }
-                if(nums[j]>nums[i])
-                {
-                    left_big++;
-                }
-            }
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i]<nums[j])
-                {
-                    right_big++;
-                }
-                if(nums[i]>nums[j])
-                {
-                    right_small++;
-                }
-            }
+            update(nums[i],1,bitr);
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            update(nums[i],-1,bitr);
+            int left_small=find(nums[i]-1,bitl);
+            int right_big=find_(nums[i]+1,bitr);
+            int left_big=find_(nums[i]+1,bitl);
+            int right_small=find(nums[i]-1,bitr);
             count+=left_small*right_big+left_big*right_small;
+            update(nums[i],1,bitl);
         }
         return count;
     }
