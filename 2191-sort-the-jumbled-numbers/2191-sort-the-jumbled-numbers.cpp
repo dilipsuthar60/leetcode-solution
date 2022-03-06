dc0e0a51c3dd87ec8daafa45df65822a939b6f7e
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-03-06 14:32:58 +0530

    Time: 1413 ms (14.29%), Space: 87.7 MB (28.57%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    static bool cmp(pair<int,pair<int,int>>&a,pair<int,pair<int,int>>&b)
+    {
+        return a.first==b.first?a.second.first<b.second.first:a.first<b.first;
+    }
+    vector<int> sortJumbled(vector<int>& map, vector<int>& nums) 
+    {
+        int count=0;
+        vector<pair<int,pair<int,int>>>v;
+        auto value=[&](string s)
+        {
+            int val=0;
+            for(int i=0;i<s.size();i++)
+            {
+                val=val*10+(s[i]-'0');
+            }
+            return val;
+        };
+        for(auto it:nums)
+        {
+            string curr="";
+            string s=to_string(it);
+            for(int i=0;i<s.size();i++)
+            {
+                curr+=to_string(map[s[i]-'0']);
+            }
+            v.push_back({value(curr),{count++,it}});
+        }
+        sort(v.begin(),v.end(),cmp);
+        vector<int>a;
+        for(auto it:v)
+        {
+            a.push_back(it.second.second);
+        }
+        return a;
+    }
+};
