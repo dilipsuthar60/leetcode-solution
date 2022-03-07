6df5121d53a9e6b5c187f8d4ca4fe02292304503
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-03-07 22:18:56 +0530

    Time: 185 ms (85.00%), Space: 66.2 MB (50.00%) - LeetHub

---
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long minimalKSum(vector<int>& nums, int k) 
+    {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        long long sum=0;
+        for(int i=0;i<n&&k;i++)
+        {
+            long long prev=(i==0)?0:nums[i-1];
+            long long missing=min(k+0ll,max(0ll,0ll+nums[i]-prev-1));
+            k-=missing;
+            sum+=(prev+1+prev+missing)*(missing)/2;
+        }
+        if(k)
+        {
+            sum+=(nums.back()+1+nums.back()+k+0ll)*k/2;
+        }
+        return sum;
+    }
+};
