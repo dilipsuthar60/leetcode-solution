160a244f8eb09930b13f1c4a962a5fcb95c41df5
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-03-14 22:11:21 +0530

    Time: 276 ms (63.29%), Space: 81.7 MB (61.45%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    void find(int x,int p,vector<vector<int>>&ans,vector<bool>&vis)
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& nums) 
     {
-        vis[x]=true;
-        for(auto it:dp[x])
-        {
-            if(vis[it]==false)
-            {
-                ans[it].push_back(p);
-                find(it,p,ans,vis);
-            }
-        }
-    }
-    vector<vector<int>> getAncestors(int n, vector<vector<int>>&nums) 
-    {
-        dp.resize(n+1);
+        vector<vector<int>>dp(n);
         for(auto &it:nums)
         {
             dp[it[0]].push_back(it[1]);
@@ -23,8 +10,23 @@ public:
         vector<vector<int>>ans(n);
         for(int i=0;i<n;i++)
         {
+            queue<int>q;
             vector<bool>vis(n,false);
-            find(i,i,ans,vis);
+            q.push(i);
+            vis[i]=true;
+            for(;q.size();q.pop())
+            {
+                auto temp=q.front();
+                for(auto child:dp[temp])
+                {
+                    if(vis[child]==false)
+                    {
+                        vis[child]=true;
+                        q.push(child);
+                        ans[child].push_back(i);
+                    }
+                }
+            }
         }
         return ans;
     }
