092661bfc95886f4564ae69360b88ad4bf0fddb1
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-03-19 21:47:32 +0530

    Time: 356 ms (40.00%), Space: 321.2 MB (40.00%) - LeetHub

---
+++ 2207-maximize-number-of-subsequences-in-a-string/2207-maximize-number-of-subsequences-in-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long find(string &s,string &t)
+    {
+        int m = t.length(), n = s.length();
+        vector<vector<long long>> dp(m + 1, vector<long long> (n + 1, 0ll));
+        for (int j = 0; j <= n; j++) dp[0][j] = 1;
+        for (int j = 1; j <= n; j++)
+            for (int i = 1; i <= m; i++)
+                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0ll);
+        return dp[m][n];
+    }
+    long long maximumSubsequenceCount(string text, string pattern) 
+    {
+        string s1=pattern[0]+text;
+        string s2=text+pattern[0];
+        string s3=pattern[1]+text;
+        string s4=text+pattern[1];
+        return max({find(s1,pattern),find(s2,pattern),find(s3,pattern),find(s4,pattern)});
+    }
+};
