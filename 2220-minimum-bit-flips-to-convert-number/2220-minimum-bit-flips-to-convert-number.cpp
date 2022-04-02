94074cf1acda329701d8f3e5ead5d18a6c7e48c7
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-04-02 22:23:27 +0530

    Time: 3 ms (14.29%), Space: 6 MB (28.57%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
-    int minBitFlips(int start, int goal) {
-        return __builtin_popcount(start^goal);
+    int minBitFlips(int start, int goal) 
+    {
+        int count=0;
+        for(int i=0;i<32;i++)
+        {
+            int first=(1<<i)&start;
+            int second=(1<<i)&goal;
+            count+=(first!=second);
+        }
+        return count;
     }
 };
