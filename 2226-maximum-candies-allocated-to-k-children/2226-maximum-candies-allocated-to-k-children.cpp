3e21d81036899092898940585c563d33045f5781
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-04-03 20:46:05 +0530

    Time: 549 ms (20.00%), Space: 84.3 MB (60.00%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool find(vector<int>&nums,long long mid,long long k)
+    {
+        int n=nums.size();
+        long long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=(1ll*nums[i]/mid);
+        }
+        return sum>=k;
+    }
+    int maximumCandies(vector<int>& nums, long long k) 
+    {
+        long long l=1;
+        int ans=0;
+        long long r=1e10;
+        while(l<=r)
+        {
+            long long mid=(l+r)/2;
+            if(find(nums,mid,k))
+            {
+                ans=mid;
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+};
