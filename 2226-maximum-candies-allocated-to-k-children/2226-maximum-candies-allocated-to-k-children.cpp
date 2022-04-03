84708d872b4e2dbb0e6f8a6967977b3e0026cc4c
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-04-03 22:40:05 +0530

    Time: 284 ms (100.00%), Space: 84.4 MB (60.00%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool find(vector<int>&nums,long long mid,long long k)
+    int maximumCandies(vector<int>& nums, long long k) 
     {
         int n=nums.size();
+        long long l=1;
+        int ans=0;
+        long long r=*max_element(nums.begin(),nums.end());
+        auto find=[&](long long mid)
+      {
         long long sum=0;
         for(int i=0;i<n;i++)
         {
             sum+=(1ll*nums[i]/mid);
         }
         return sum>=k;
-    }
-    int maximumCandies(vector<int>& nums, long long k) 
-    {
-        long long l=1;
-        int ans=0;
-        long long r=1e18;
+    };
         while(l<=r)
         {
             long long mid=(l+r)/2;
-            if(find(nums,mid,k))
+            if(find(mid))
             {
                 ans=mid;
                 l=mid+1;
