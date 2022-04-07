71b786a53bdeed2297f45e8cc25de1f1d3ec831a
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-04-07 23:24:00 +0530

    Time: 804 ms (64.57%), Space: 41.1 MB (60.72%) - LeetHub

---
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int n;
+    int dp[1011][1011];
+    int find(string &s,vector<int>&prefix,int index,int num,int len)
+    {
+        if(index>=n)
+        {
+            return 0;
+        }
+        if(num==0)
+        {
+            return 0;
+        }
+        if(dp[index][num]!=-1)
+        {
+            return dp[index][num];
+        }
+        int one=prefix[min(n-1,index+len-1)]-(index?prefix[index-1]:0);
+        int take=one+find(s,prefix,index+len,num-1,len);
+        int not_take=find(s,prefix,index+1,num,len);
+        return dp[index][num]=max(take,not_take);
+    }
+    int minimumWhiteTiles(string s, int num, int len) 
+    {
+        memset(dp,-1,sizeof(dp));
+        n=s.size();
+        vector<int>prefix(n);
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='1')
+            {
+                c++;
+            }
+            prefix[i]=c;
+        }
+        int val=find(s,prefix,0,num,len);
+        return prefix[n-1]-val;
+    }
+};
