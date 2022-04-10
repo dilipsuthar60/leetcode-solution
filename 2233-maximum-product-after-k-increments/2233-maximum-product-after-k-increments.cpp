b8d7838d74e662a60b90c79004948b42435e9075
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-04-10 19:54:55 +0530

    Time: 361 ms (30.00%), Space: 82.2 MB (20.00%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
@@ -1,35 +1,62 @@
 class Solution {
 public:
+    int mod=1e9+7;
+    long long find(vector<int>&nums,long long mid,int n,int k)
+    {
+        
+            long long  count=0;
+            for(int i=0;i<n;i++)
+            {
+                if(nums[i]<mid)
+                {
+                    count+=(mid-nums[i]);
+                }
+            }
+            if(count>k)
+            {
+                return -1;
+            }
+            long long pro=1;
+            for(int i=0;i<n;i++)
+            {
+                if(mid<nums[i])
+                {
+                    pro=(pro*nums[i])%mod;
+                }
+                else
+                {
+                    if(count<k)
+                    {
+                        count++;
+                        pro=(pro*(mid+1))%mod;
+                    }
+                    else
+                    {
+                        pro=(pro*mid)%mod;
+                    }
+                }
+            }
+            return pro;
+    }
     int maximumProduct(vector<int>& nums, int k) 
     {
         int n=nums.size();
-        if(n==1)
+        int l=0;
+        int r=1e7;
+        int ans=0;
+        while(l<=r)
         {
-            return nums[0]+k;
-        }
-        priority_queue<long long ,vector<long long>,greater<long long>>pq;
-        for(auto &it:nums)
-        {
-            pq.push(it);
-        }
-        while(k)
-        {
-            auto temp1=pq.top();
-            pq.pop();
-            auto temp2=pq.top();
-            pq.pop();
-            int diff=min(k+0ll,(temp2-temp1+1));
-            k-=diff;
-            temp1+=diff;
-            pq.push(temp1);
-            pq.push(temp2);
-        }
-        int mod=1e9+7;
-        long long ans=1;
-        while(pq.size())
-        {
-            ans=(ans*pq.top())%mod;
-            pq.pop();
+            long long mid=(l+r)/2;
+            long long val=find(nums,mid,n,k);
+            if(val==-1)
+            {
+                r=mid-1;
+            }
+            else
+            {
+                ans=val;
+                l=mid+1;
+            }
         }
         return (int)ans%mod;
     }
