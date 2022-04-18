bfd48f95990f6ed364de10d6eb9df32da54b9e86
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-04-18 23:18:35 +0530

    Time: 408 ms (100.00%), Space: 177.8 MB (100.00%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,54 +1,41 @@
 class Solution {
 public:
-    int longestPath(vector<int>& parent, string s) {
-        
-        int n = parent.size();
-        vector<vector<int>>adj(n+1);
-        for(int i = 1; i<n; i++){
-            int nxt = parent[i];
-            if(s[nxt]!=s[i]){
-                int x = i; int y = nxt;
-                adj[x].push_back(y);
-                adj[y].push_back(x);
-            }
-        }
-        int ans = 0;
-        queue<int>q;
-        vector<bool>vis(n+1);
-        vector<bool>vis2(n+1);
-        vector<int>dis(n+1);
-        vector<int>dis2(n+1);
-        for(int i = 1; i<=n; i++){
-            if(vis[i])continue;
-            vis[i] = true;
-            q.push(i);
-            int dist = 0; int node = 1;
-            while(q.size()){
-                int cur = q.front(); q.pop();
-                for(int nxt: adj[cur]){
-                    if(vis[nxt])continue;
-                    vis[nxt] = true;
-                    q.push(nxt);
-                    dis[nxt] = dis[cur]+1;
-                    if(dis[nxt]>dist){
-                        dist = dis[nxt];
-                        node = nxt;
-                    }
+    vector<vector<int>>dp;
+    int ans=0;
+    int find(int x,string &s)
+    {
+        int max1=0;
+        int max2=0;
+        for(auto it:dp[x])
+        {
+            int len=find(it,s);
+            if(s[x]!=s[it])
+            {
+                if(len>=max1)
+                {
+                    max2=max1;
+                    max1=len;
                 }
-            }
-            q.push(node);
-            vis2[node] = true;
-            while(q.size()){
-                int cur = q.front(); q.pop();
-                for(int nxt: adj[cur]){
-                    if(vis2[nxt])continue;
-                    dis2[nxt] = dis2[cur]+1;
-                    q.push(nxt);
-                    vis2[nxt] = true;
-                    ans = max(ans,dis2[nxt]);
+                else if(len>=max2)
+                {
+                    max2=len;
                 }
             }
         }
-        return ans+1;
+        // sort(v.rbegin(),v.rend());
+        ans=max(ans,max1+max2+1);
+        return max1+1;
+    }
+    int longestPath(vector<int>& parent, string s) 
+    {
+          ans=0;
+        int n=parent.size();
+        dp.resize(n+1);
+        for(int i=1;i<n;i++)
+        {
+            dp[parent[i]].push_back(i);
+        }
+        find(0,s);
+        return ans;
     }
 };
