5c0c70171474e95e817e11073c201a9728a3f412
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-06 19:15:35 +0530

    Time: 34 ms (50.41%), Space: 10.4 MB (57.07%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    class node
+    {
+        public:
+        char ch;
+        int f;
+        node(char ch,int f)
+        {
+            this->ch=ch;
+            this->f=f;
+        }
+    };
+    string removeDuplicates(string s, int k) 
+    {
+        int n=s.size();
+        stack<node>st;
+        node first('#',1);
+        st.push(first);
+        for(int i=0;i<n;i++)
+        {
+            if(st.top().ch!=s[i])
+            {
+                st.push(node(s[i],1));
+            }
+            else if(++st.top().f==k)
+            {
+                st.pop();
+            }
+        }
+        string ans="";
+        while(st.size())
+        {
+            int freq=st.top().f;
+            char chat=st.top().ch;
+            ans+=string(freq,chat);
+            st.pop();
+        }
+        ans.pop_back();
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
