1b00d7123eaa4643d26c3f3dc379d7172c62a584
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-08 21:34:16 +0530

    Time: 8 ms (100.00%), Space: 12 MB (25.00%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -1,36 +1,38 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        pair<int, int> find(TreeNode *root, int &sum)
-        {
-            if (root == NULL)
-            {
-                return { 0,
-                    0 };
-            }
-            auto p1 = find(root->left, sum);
-            auto p2 = find(root->right, sum);
-            if (((p1.first + p2.first + root->val) / (p1.second + p2.second + 1)) == root->val)
-            {
-                sum++;
-            };
-            return {p1.first + p2.first + root->val,p1.second + p2.second + 1};
-        }
-    int averageOfSubtree(TreeNode *root)
+class Solution {
+public:
+    pair<int,int>find(TreeNode*root,int&sum)
     {
-        int sum = 0;
-        find(root, sum);
+        if(root==NULL)
+        {
+            //first sum of subtree
+            // second size of subtree
+            return {0,0};
+        }
+        auto left=find(root->left,sum);
+        auto right=find(root->right,sum);
+        int sum_of_subtree=root->val+left.first+right.first;
+        int size_of_subtree=1+left.second+right.second;
+        if((sum_of_subtree)/(size_of_subtree)==root->val)
+        {
+            sum++;
+        }
+        return {sum_of_subtree,size_of_subtree};
+    }
+    int averageOfSubtree(TreeNode* root) 
+    {
+        int sum=0;
+        find(root,sum);
         return sum;
     }
 };
