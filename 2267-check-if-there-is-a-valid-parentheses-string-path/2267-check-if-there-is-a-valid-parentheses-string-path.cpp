ac3eb4b868aa01ced725b62a981ea4e64649c947
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-08 09:48:55 +0530

    Time: 165 ms (25.00%), Space: 21.9 MB (75.00%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -1,61 +1,120 @@
 class Solution {
 public:
     int n,m;
-    bool hasValidPath(vector<vector<char>>& mat) 
+    int dp[101][101][200];
+    bool find(vector<vector<char>>& mat,int row,int col,int open)
     {
-        vector<pair<int,int>>d={{0,1},{1,0}};
-        n=mat.size();
-        m=mat[0].size();
-        queue<tuple<int,int,int>>q;
-        if(mat[0][0]=='(')
-        {
-        q.push({0,0,1});
-        }
-        else
+        if(row<0||col<0||row>=n||col>=m||open<0)
         {
             return false;
         }
-        int dp[111][111][311];
-        memset(dp,-1,sizeof(dp));
-        dp[0][0][1]=1;
-        while(q.size())
+        if(dp[row][col][open]!=-1)
         {
-            auto [x,y,b]=q.front();
-            q.pop();
-            if(b<0)
+            return dp[row][col][open];
+        }
+        if(row==n-1&&col==m-1)
+        {
+            if(mat[row][col]=='(')
             {
-                continue;
+                open++;
             }
-            if(x==n-1&&y==m-1)
+            else 
             {
-                if(b==0)
-                {
-                    return true;
-                }
+                open--;
             }
-            for(auto it:d)
+            return open==0;
+        }
+        if(mat[row][col]=='(')
+        {
+            if(find(mat,row+1,col,open+1))
             {
-                int nx=x+it.first;
-                int ny=y+it.second;
-                if(b<0)
-                {
-                    continue;
-                }
-                if(nx>=0&&ny>=0&&nx<n&&ny<m&&dp[nx][ny][b]==-1)
-                {
-                    if(b>=0)
-                    dp[nx][ny][b]=1;
-                    if(mat[nx][ny]=='(')
-                    {
-                        q.push({nx,ny,b+1});
-                    }
-                    else
-                    {
-                        q.push({nx,ny,b-1});
-                    }
-                }
+                return true;
+            }
+            if(find(mat,row,col+1,open+1))
+            {
+                return true;
             }
         }
-        return false;
+        else
+        {
+            if(find(mat,row+1,col,open-1))
+            {
+                return true;
+            }
+            if(find(mat,row,col+1,open-1))
+            {
+                return true;
+            }
+        
+        }
+        return dp[row][col][open]=false;
+    }
+    bool hasValidPath(vector<vector<char>>& mat) 
+    {
+        memset(dp,-1,sizeof(dp));
+        n=mat.size();
+        m=mat[0].size();
+        return find(mat,0,0,0);
     }
 };
+// class Solution {
+// public:
+//     int n,m;
+//     bool hasValidPath(vector<vector<char>>& mat) 
+//     {
+//         vector<pair<int,int>>d={{0,1},{1,0}};
+//         n=mat.size();
+//         m=mat[0].size();
+//         queue<tuple<int,int,int>>q;
+//         if(mat[0][0]=='(')
+//         {
+//         q.push({0,0,1});
+//         }
+//         else
+//         {
+//             return false;
+//         }
+//         int dp[111][111][311];
+//         memset(dp,-1,sizeof(dp));
+//         dp[0][0][1]=1;
+//         while(q.size())
+//         {
+//             auto [x,y,b]=q.front();
+//             q.pop();
+//             if(b<0)
+//             {
+//                 continue;
+//             }
+//             if(x==n-1&&y==m-1)
+//             {
+//                 if(b==0)
+//                 {
+//                     return true;
+//                 }
+//             }
+//             for(auto it:d)
+//             {
+//                 int nx=x+it.first;
+//                 int ny=y+it.second;
+//                 if(b<0)
+//                 {
+//                     continue;
+//                 }
+//                 if(nx>=0&&ny>=0&&nx<n&&ny<m&&dp[nx][ny][b]==-1)
+//                 {
+//                     if(b>=0)
+//                     dp[nx][ny][b]=1;
+//                     if(mat[nx][ny]=='(')
+//                     {
+//                         q.push({nx,ny,b+1});
+//                     }
+//                     else
+//                     {
+//                         q.push({nx,ny,b-1});
+//                     }
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
