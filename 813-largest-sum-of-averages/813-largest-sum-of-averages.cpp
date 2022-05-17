184a050ae61f37cd4e88ada25f8e90baeeda0828
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-17 17:49:20 +0530

    Time: 26 ms (50.09%), Space: 7.4 MB (88.83%) - LeetHub

---
+++ 813-largest-sum-of-averages/813-largest-sum-of-averages.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double dp[110][110];
+    double find(vector<int>&nums,int index,int k)
+    {
+        if(index>=nums.size()||k<0)
+        {
+            return 0;
+        }
+        if(k==0)
+        {
+            return INT_MIN/2;
+        }
+        if(dp[index][k]!=0)
+        {
+            return dp[index][k];
+        }
+        double sum=0;
+        double ans=0;
+        for(int i=index;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            ans=max(ans,sum/(i-index+1)+find(nums,i+1,k-1));
+        }
+        return dp[index][k]=ans;
+    }
+    double largestSumOfAverages(vector<int>& nums, int k) 
+    {
+        memset(dp,0,sizeof(dp));
+        return find(nums,0,k);
+    }
+};
