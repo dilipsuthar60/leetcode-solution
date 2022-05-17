ff9d1223a6aef6d3896127fa3eaa8ca32122581f
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-17 20:42:21 +0530

    Time: 493 ms (62.51%), Space: 71.1 MB (60.13%) - LeetHub

---
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int maximumWhiteTiles(vector<vector<int>>&nums, int len) 
+    {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int>sum(n);
+        for(int i=0;i<n;i++)
+        {
+            sum[i]=nums[i][1]-nums[i][0]+1;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(i)
+            {
+                sum[i]+=sum[i-1];
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int st=nums[i][0];
+            int ed=nums[i][1];
+            int req=st+len-1;
+            int l=i;
+            int r=n-1;
+            int index=i;
+            while(l<=r)
+            {
+                int mid=(l+r)/2;
+                if(nums[mid][0]<=req)
+                {
+                    l=mid+1;
+                    index=mid;
+                }
+                else
+                {
+                    r=mid-1;
+                }
+            }
+            int tile=sum[index];
+            if(i)
+            {
+                tile-=sum[i-1];
+            }
+            if(nums[index][1]>req)
+            {
+                tile-=(nums[index][1]-req);
+            }
+            ans=max(ans,tile);
+        }
+        return ans;
+    }
+};
