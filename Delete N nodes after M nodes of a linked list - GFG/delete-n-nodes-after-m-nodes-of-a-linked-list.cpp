5e128895a3966760d7b57767416a4bd1bc7644e8
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-18 23:08:29 +0530

    Added solution - LeetHub

---
+++ Delete N nodes after M nodes of a linked list - GFG/delete-n-nodes-after-m-nodes-of-a-linked-list.cpp
@@ -0,0 +1,108 @@
+// { Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while(node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert(int n1)
+{
+    int n,value;
+    n=n1;
+    struct Node *temp;
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp=start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp=temp->next;
+        }
+    }
+}
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+    void linkdelete(struct Node  *head, int m, int n)
+    {
+        Node*curr=new Node(-1);
+        curr=head;
+        Node*pre;
+        while(curr!=NULL)
+        {
+           int i=m;
+           int j=n;
+           while(curr!=NULL&&i>0)
+           {
+               pre=curr;
+               curr=curr->next;
+               i--;
+           }
+           while(curr!=NULL&&j>0)
+           {
+               j--;
+               curr=curr->next;
+           }
+           pre->next=curr;
+        }
+    }
+};
+
+
+
+// { Driver Code Starts.
+int main()
+{
+    int t,n1;
+    cin>>t;
+    while (t--) {
+        cin>>n1;
+        int m,n;
+        cin>>m;
+        cin>>n;
+        insert(n1);
+        Solution ob;
+        ob.linkdelete(start,m,n);
+        printList(start);
+    }
+    
+    return 0;
+}
+  // } Driver Code Ends
