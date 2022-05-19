1d9aa8a4bb466e479da4fa6acc04cd7a914659f2
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-19 13:19:10 +0530

    Added solution - LeetHub

---
+++ Largest number possible - GFG/largest-number-possible.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    string findLargest(int n, int sum)
+    {
+        if(n==1&&sum==0)
+        {
+            return "0";
+        }
+        if(n&&sum==0)
+        {
+            return "-1";
+        }
+        if(n*9<sum)
+        {
+            return "-1";
+        }
+        string s=string(n,'9');
+        int A=n*9;
+        for(int i=n-1;i>=0;i--)
+        {
+            int diff=max(-9,sum-A);
+            s[i]+=diff;
+            A+=diff;
+        }
+        return s;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, S;
+        cin>>N>>S;
+        
+        Solution ob;
+        cout<<ob.findLargest(N, S)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
