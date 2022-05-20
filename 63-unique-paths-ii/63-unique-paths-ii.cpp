2feff0575951f74ffcfcac14f68c3da26ba062a8
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-20 18:21:42 +0530

    Time: 3 ms (80.74%), Space: 7.8 MB (41.40%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& mat) 
+    {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(mat[i-1][j-1]==0)
+                {
+                    dp[i][j]=(i==1&&j==1)?1:dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[n][m];
+                          
+    }
+};
