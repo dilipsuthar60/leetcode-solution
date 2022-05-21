adeab847731094df7d4e5fc57d083101635483b6
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-21 19:43:32 +0530

    Time: 18 ms (90.00%), Space: 8.3 MB (6.40%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int find(string s,string t)
+    {
+        int n=s.size();
+        int m=t.size();
+        int dp1[256]={0};
+        int dp2[256]={0};
+        int j=0;
+        for(int i=0;i<m;i++)
+        {
+            dp1[t[i]]++;
+        }
+        int len=INT_MAX;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            dp2[s[i]]++;
+            if(dp1[s[i]]!=0&&dp2[s[i]]<=dp1[s[i]])
+            {
+                count++;
+            }
+            if(count==m)
+            {
+                while(j<=i&&((dp1[s[j]]==0)||dp2[s[j]]>dp1[s[j]]))
+                {
+                    dp2[s[j]]--;
+                    j++;
+                }
+                int window=(i-j+1);
+                len=min(len,window);
+            }
+        }
+        return len;
+    }
+    int balancedString(string s) 
+    {
+        map<char,int>mp;
+        for(auto &it:s)
+        {
+            mp[it]++;
+        }
+        int n=s.size();
+        string t="";
+        for(auto&[ch,f]:mp)
+        {
+            if(f>n/4)
+            {
+                t+=string(f-n/4,ch);
+            }
+        }
+        return find(s,t);
+    }
+};
