defa7a480ba3a807db2727afdcfc2411f3b5509a
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-26 22:42:30 +0530

    Time: 5 ms (69.03%), Space: 7 MB (82.44%) - LeetHub

--- 1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
+++ 1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    vector<pair<int,int>>d={{0,-1},{1,0},{0,1},{-1,0}};
+    vector<pair<int,int>>d={{0,0},{0,-1},{1,0},{0,1},{-1,0}};
     int minFlips(vector<vector<int>>& mat) 
     {
         int n=mat.size();
         int m=mat[0].size();
-        set<vector<vector<int>>>vis;
-        queue<vector<vector<int>>>q;
-        q.push(mat);
+        set<int>vis;
+        queue<int>q;
+        int mask=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(mat[i][j])
+                mask|=(mat[i][j]<<(i*m+j));
+            }
+        }
+        q.push(mask);
         int level=0;
-        auto find=[&](vector<vector<int>>&mat)
-        {
-            int count=0;
-            for(auto it:mat)
-            {
-                for(auto t:it)
-                {
-                    count+=(t==0);
-                }
-            }
-            return count==n*m;
-        };
-        auto flip=[&](vector<vector<int>>&mat,int x,int y)
-        {
-            mat[x][y]^=1;
-            for(auto it:d)
-            {
-                int nx=x+it.first;
-                int ny=y+it.second;
-                if(nx>=0&&ny>=0&&nx<n&&ny<m)
-                {
-                    mat[nx][ny]^=1;
-                }
-            }
-        };
         while(q.size())
         {
             int size=q.size();
@@ -41,7 +25,7 @@ public:
             {
             auto temp=q.front();
             q.pop();
-            if(find(temp))
+            if(temp==0)
             {
                 return level;
             }
@@ -49,12 +33,20 @@ public:
             {
                 for(int j=0;j<m;j++)
                 {
-                    vector<vector<int>>m=temp;
-                    flip(m,i,j);
-                    if(vis.find(m)==vis.end())
+                   int curr=temp;
+                    for(auto it:d)
                     {
-                        q.push(m);
-                        vis.insert(m);
+                        int nx=i+it.first;
+                        int ny=j+it.second;
+                        if(nx>=0&&ny>=0&&nx<n&&ny<m)
+                        {
+                           curr^=(1<<(nx*m+ny));
+                        }
+                    }
+                    if(vis.find(curr)==vis.end())
+                    {
+                        q.push(curr);
+                        vis.insert(curr);
                     }
                 }
             }
