ea57892688bdb193d896c5880f828d97abd77e38
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-28 22:11:36 +0530

    Time: 497 ms (100.00%), Space: 113.9 MB (50.00%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& nums)
     {
-        vector<long long>dp(n);
+        vector<long long>dp(n+1);
         for(auto &it:nums)
         {
             dp[it[0]]++;
             dp[it[1]]++;
         }
         long long sum=0;
-       sort(dp.begin(),dp.end());
+       sort(dp.begin(),dp.end()-1);
         for(int i=0;i<n;i++)
         {
             sum+=(i+1)*(dp[i]);
