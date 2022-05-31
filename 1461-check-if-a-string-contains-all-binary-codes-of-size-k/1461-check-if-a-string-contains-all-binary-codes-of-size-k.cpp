a781450ea4bea47b6f3c5225be4c2b67c654cd24
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-05-31 11:29:44 +0530

    Time: 71 ms (91.51%), Space: 22.1 MB (88.99%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -4,18 +4,20 @@ public:
     {
         
         int size=1<<k;
-        unordered_set<int>st;
+        int dp[size+1];
+        memset(dp,-1,sizeof(dp));
         int allone=size-1;
         int hash=0;
-        
+        int count=0;
         for(int i=0,n=s.size();i<n;i++)
         {
             hash=((hash<<1)&(allone))|(s[i]-'0');
-            if(i>=k-1)
+            if(i>=k-1&&dp[hash]==-1)
             {
-                st.insert(hash);
+                dp[hash]=1;
+                count++;
             }
         }
-        return st.size()==size;
+        return count==size;
     }
 };
