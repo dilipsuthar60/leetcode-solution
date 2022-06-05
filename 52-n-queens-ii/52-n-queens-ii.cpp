2788b9dff1e612ed724d8fecca9a07551f6c5e51
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-05 07:56:11 +0530

    Time: 7 ms (58.17%), Space: 6.2 MB (61.83%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int count;
+      bool is_safe(vector<string>&v,int row,int col)
+    {
+        for(int i=row-1;i>=0;i--)
+        {
+            if(v[i][col]=='Q')
+            {
+                return false;
+            }
+        }
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--)
+        {
+            if(v[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+         for(int i=row-1,j=col+1;i>=0&&j<v.size();i--,j++)
+        {
+            if(v[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void find(int n,vector<string>&v,int row)
+    {
+        if(row==n)
+        {
+            count++;
+          
+            return ;
+        }
+        for(int col=0;col<n;col++)
+        {
+            if(is_safe(v,row,col))
+            {
+                v[row][col]='Q';
+                find(n,v,row+1);
+                v[row][col]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) 
+    {
+         count=0;
+          vector<vector<string>>ans;
+        vector<string>v(n,string(n,'.'));
+        find(n,v,0);
+        return count;
+    }
+};
