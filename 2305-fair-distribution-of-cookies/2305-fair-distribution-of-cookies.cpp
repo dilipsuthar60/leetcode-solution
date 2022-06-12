9ce8371f3826de8b2eac99fd3854d18c2a4aafe5
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-12 11:08:47 +0530

    Time: 600 ms (50.00%), Space: 6.9 MB (100.00%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,25 +1,55 @@
 class Solution {
 public:
-    int ans=1e8;
-    void find(vector<int>&nums,vector<int>&v,int k,int index)
+    int n;
+    bool find(vector<int>&nums,int k,int mid)
     {
-        if(index>=nums.size())
+        int count=1;
+        int sum=0;
+        for(int i=0;i<n;i++)
         {
-            int mx=*max_element(v.begin(),v.end());
-            ans=min(ans,mx);
-            return ;
+            sum+=nums[i];
+            if(mid<nums[i])
+            {
+                return false;
+            }
+            if(sum>mid)
+            {
+                count++;
+                sum=nums[i];
+            }
         }
-        for(int i=0;i<k;i++)
-        {
-            v[i]+=nums[index];
-            find(nums,v,k,index+1);
-            v[i]-=nums[index];
-        }
+        return count<=k;
     }
-    int distributeCookies(vector<int>&nums, int k) 
+    int bs(vector<int>&nums,int k)
     {
-        vector<int>v(k);
-        find(nums,v,k,0);
+        int l=0;
+        int ans=0;
+        int r=1e6;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(find(nums,k,mid))
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+    int distributeCookies(vector<int>& nums, int k) 
+    {
+        n=nums.size();
+        int ans=1e8;
+        sort(nums.begin(),nums.end());
+        do
+        {
+         ans=min(ans,bs(nums,k));   
+        }while(next_permutation(nums.begin(),nums.end()));
+       
         return ans;
     }
 };
