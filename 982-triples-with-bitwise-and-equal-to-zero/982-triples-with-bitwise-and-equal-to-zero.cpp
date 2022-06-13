73d6280199664429b26316b81bdcfdbede1850df
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-13 22:55:30 +0530

    Time: 1213 ms (5.82%), Space: 7.4 MB (89.63%) - LeetHub

---
+++ 982-triples-with-bitwise-and-equal-to-zero/982-triples-with-bitwise-and-equal-to-zero.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countTriplets(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int dp[1<<16];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                dp[nums[i]&nums[j]]++;
+            }
+        }
+        int count=0;
+        for(int k=0;k<n;k++)
+        {
+            for(int i=0;i<(1<<16);i++)
+            {
+                if((i&nums[k])==0)
+                {
+                    count+=dp[i];
+                }
+            }
+        }
+        return count;
+    }
+};
