1df47d031fb741a45ce4bc9c36a1625afe9b6897
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-14 00:01:53 +0530

    Time: 1412 ms (14.05%), Space: 129.5 MB (58.08%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    class node
+    {
+        public:
+        int end_time;
+        int index;
+        node(int end_time,int index)
+        {
+            this->end_time=end_time;
+            this->index=index;
+        }
+    };
+    class cmp
+    {
+        public:
+            bool operator()(node&a, node&b)
+            {
+                if(a.end_time==b.end_time)
+                {
+                    return a.index>b.index;
+                }
+                return a.end_time>b.end_time;
+            }
+    };
     vector<int> getOrder(vector<vector<int>>&nums)
     {
         int n=nums.size();
@@ -8,7 +31,7 @@ public:
             nums[i].push_back(i);
         }
         sort(nums.begin(),nums.end());
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        priority_queue<node,vector<node>,cmp>pq;
         int i=0;
         long long time=0;
         vector<int>ans;
@@ -20,10 +43,12 @@ public:
             }
             while(i<n&&nums[i][0]<=time)
             {
-                pq.push({nums[i][1],nums[i][2]});
+                pq.push(node(nums[i][1],nums[i][2]));
                 i++;
             }
-            auto [burst,index]=pq.top();
+            node current_node=pq.top();
+            int burst=current_node.end_time;
+            int index=current_node.index;
             pq.pop();
             ans.push_back(index);
             time+=burst;
