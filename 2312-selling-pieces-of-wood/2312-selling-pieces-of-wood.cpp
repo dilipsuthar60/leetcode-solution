b86d0ff912ce9080369fd6a22c6c29b72f3537ce
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-19 18:31:57 +0530

    Time: 426 ms (100.00%), Space: 25.2 MB (100.00%) - LeetHub

---
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long cost[201][201];
+    long long dp[201][201];
+    long long find(int n,int m)
+    {
+        if(dp[n][m]!=-1)
+        {
+            return dp[n][m];
+        }
+        long long ans=cost[n][m];
+        for(int i=1;i<n;i++)
+        {
+            ans=max(ans,find(i,m)+find(n-i,m));
+        }
+        for(int j=1;j<m;j++)
+        {
+            ans=max(ans,find(n,j)+find(n,m-j));
+        }
+        return dp[n][m]=ans;
+    }
+    long long sellingWood(int n, int m, vector<vector<int>>& prices) 
+    {
+        memset(cost,0,sizeof(cost));
+        memset(dp,-1,sizeof(dp));
+        for(auto &it:prices)
+        {
+            cost[it[0]][it[1]]=it[2];
+        }
+        return find(n,m);
+    }
+};
