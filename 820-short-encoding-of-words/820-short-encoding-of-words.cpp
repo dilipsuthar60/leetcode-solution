00b933f38341c1408cb59c1d0e20779959151612
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-20 14:47:41 +0530

    Time: 238 ms (13.37%), Space: 18.4 MB (65.84%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>&nums)
+    {
+        set<string>s(nums.begin(),nums.end());
+        for(auto it:nums)
+        {
+            int n=it.size();
+            for(int i=1;i<n;i++)
+            {
+                s.erase(it.substr(i));
+            }
+        }
+        int count=0;
+        for(auto it:s)
+        {
+            count+=it.size()+1;
+        }
+        return count;
+    }
+};
