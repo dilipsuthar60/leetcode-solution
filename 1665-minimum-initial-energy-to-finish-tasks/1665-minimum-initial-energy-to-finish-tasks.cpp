e0c2a5c8c6fe9575f27e4b78844dc705b8c57ba3
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-21 17:23:05 +0530

    Time: 814 ms (26.57%), Space: 106.4 MB (12.61%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool find(vector<vector<int>>&nums,long long mid)
+    {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i][1]<=mid)
+            {
+                mid-=nums[i][0];
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    int minimumEffort(vector<vector<int>>&nums) 
+    {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            nums[i].push_back(nums[i][1]-nums[i][0]);
+        }
+        sort(nums.begin(),nums.end(),[&](auto &a,auto &b){return a[2]>b[2];});
+        long long l=0;
+        long long r=1e18;
+        int ans=0;
+        while(l<=r)
+        {
+            long long mid=(l+r)/2;
+            if(find(nums,mid))
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
