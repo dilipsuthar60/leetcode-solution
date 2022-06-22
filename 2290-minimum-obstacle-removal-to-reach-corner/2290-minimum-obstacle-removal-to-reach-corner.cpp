31cf3e1c52e3ab3d67722df56c8426dd58a2afa0
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-22 22:46:23 +0530

    Time: 1958 ms (6.45%), Space: 160.2 MB (27.16%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-    int minimumObstacles(vector<vector<int>>& mat)
-    {
-        vector<pair<int,int>>d={{-1,0},{0,1},{1,0},{0,-1}};
+    int minimumObstacles(vector<vector<int>>& mat) {
+         vector<pair<int,int>>d={{-1,0},{0,1},{1,0},{0,-1}};
         int n=mat.size();
         int m=mat[0].size();
-        queue<int>q;
-        q.push(0);
-        int dp[n][m];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                dp[i][j]=1e5+10;
-            }
-        }
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
         dp[0][0]=0;
         while(q.size())
         {
@@ -23,8 +15,8 @@ public:
             {
                 auto temp=q.front();
                 q.pop();
-                int x=temp/m;
-                int y=temp%m;
+                int x=temp.first;
+                int y=temp.second;
                 for(auto it:d)
                 {
                     int nx=x+it.first;
@@ -32,11 +24,12 @@ public:
                     if(nx>=0&&ny>=0&&nx<n&&ny<m&&dp[x][y]+mat[nx][ny]<dp[nx][ny])
                     {
                         dp[nx][ny]=dp[x][y]+mat[nx][ny];
-                        q.push(nx*m+ny);
+                         q.push({nx,ny});
                     }
                 }
             }
         }
-        return dp[n-1][m-1];      
+        return dp[n-1][m-1];
+                
     }
 };
