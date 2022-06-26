c80dbe9ba07f1610aaefa73b2a54739b19f3fe1e
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-26 18:39:51 +0530

    Time: 3 ms (53.53%), Space: 7.7 MB (75.26%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,23 +3,29 @@ public:
     int rob(vector<int>& nums)    
     {
         int n=nums.size();
-        int dp[n];
-        memset(dp,0,sizeof(dp));
+        if(n==1)
+        {
+            return nums[0];
+        }
+        int prev1=0;
+        int prev2=0;
         for(int i=0;i<n;i++)
         {
             if(i==0)
             {
-                dp[i]=nums[i];
+               prev2=nums[i];
             }
             else if(i==1)
             {
-                dp[i]=max(nums[i],nums[i-1]);
+                prev1=max(nums[i],nums[i-1]);
             }
             else
             {
-                dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+                int temp=prev1;
+                prev1=max(nums[i]+prev2,prev1);
+                prev2=temp;
             }
         }
-        return dp[n-1];
+        return prev1;
     }
 };
