7364946fe470815d144a50f34825a5042c7cefca
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-28 16:43:16 +0530

    Time: 95 ms (49.37%), Space: 17.2 MB (81.59%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int minDeletions(string s)
+    int minDeletions(string s) 
     {
         int n=s.size();
-        vector<int>v(26,0);
+        int dp[26]={0};
         for(int i=0;i<n;i++)
         {
-            v[s[i]-97]++;
+            dp[s[i]-'a']++;
         }
-        sort(v.rbegin(),v.rend());
-        int count=0;
+        int del=0;
         int max_freq=s.size();
-        for(int i=0;i<26&&v[i]>0;i++)
+        sort(dp,dp+26);
+        for(int i=25;i>=0&&dp[i]>0;i--)
         {
-            if(v[i]>max_freq)
+            if(dp[i]>max_freq)
             {
-                count+=v[i]-max_freq;
-                v[i]=max_freq;
+                del+=dp[i]-max_freq;
+                dp[i]=max_freq;
             }
-            max_freq=max(0,v[i]-1);
+            max_freq=max(0,dp[i]-1);
         }
-        return count;
+        return del;
     }
 };
