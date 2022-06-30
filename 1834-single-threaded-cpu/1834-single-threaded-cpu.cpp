3847525a2e84eef65fbce77a8fa592664c4de901
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-06-30 22:29:54 +0530

    Time: 1155 ms (30.32%), Space: 129.5 MB (47.01%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,28 +1,5 @@
 class Solution {
 public:
-    class node
-    {
-        public:
-        int end_time;
-        int index;
-        node(int end_time,int index)
-        {
-            this->end_time=end_time;
-            this->index=index;
-        }
-    };
-    class cmp
-    {
-        public:
-            bool operator()(node&a, node&b)
-            {
-                if(a.end_time==b.end_time)
-                {
-                    return a.index>b.index;
-                }
-                return a.end_time>b.end_time;
-            }
-    };
     vector<int> getOrder(vector<vector<int>>&nums)
     {
         int n=nums.size();
@@ -31,26 +8,22 @@ public:
             nums[i].push_back(i);
         }
         sort(nums.begin(),nums.end());
-        priority_queue<node,vector<node>,cmp>pq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         int i=0;
         long long time=nums[0][0];
         vector<int>ans;
         while(i<n||pq.size())
         {
-            while(i<n&&nums[i][0]<=time)
-            {
-                pq.push(node(nums[i][1],nums[i][2]));
-                i++;
-            }
             if(pq.size()==0)
             {
-                time=nums[i][0];
-                pq.push(node(nums[i][1],nums[i][2]));
+                time=max(time,0ll+nums[i][0]);
+            }
+            while(i<n&&nums[i][0]<=time)
+            {
+                pq.push({nums[i][1],nums[i][2]});
                 i++;
             }
-            node current_node=pq.top();
-            int burst=current_node.end_time;
-            int index=current_node.index;
+            auto [burst,index]=pq.top();
             pq.pop();
             ans.push_back(index);
             time+=burst;
