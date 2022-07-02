9cab0b0a1607447e1061361d06ab6974519c6d95
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-02 20:08:58 +0530

    Time: 73 ms (80.95%), Space: 25.4 MB (53.46%) - LeetHub

--- 475-heaters/475-heaters.cpp
+++ 475-heaters/475-heaters.cpp
@@ -5,9 +5,32 @@ public:
         sort(nums.begin(),nums.end());
         int ans=0;
         int n=houses.size();
+        auto lowerbound=[&](int val)
+        {
+            int l=0;
+            int r=nums.size()-1;
+            int index=0;
+            while(l<=r)
+            {
+                int mid=(l+r)>>1;
+                if(nums[mid]>val)
+                {
+                    r=mid-1;
+                }
+                else
+                {
+                    l=mid+1;
+                }
+            }
+            if(r<0)
+            {
+                return 0;
+            }
+            return nums[r]>val?r:r+1;
+        };
         for(int i=0;i<n;i++)
         {
-            int index=lower_bound(nums.begin(),nums.end(),houses[i])-nums.begin();
+            int index=lowerbound(houses[i]);
             int curr=INT_MAX;
             if(index<nums.size())
             {
