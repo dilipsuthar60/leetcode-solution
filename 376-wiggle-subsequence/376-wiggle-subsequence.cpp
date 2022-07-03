b888495fe7a0978a8d22e06ad95a4b36631e40ed
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-03 16:24:31 +0530

    Time: 6 ms (43.60%), Space: 7.1 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -3,30 +3,19 @@ public:
     int wiggleMaxLength(vector<int>& nums) 
     {
         int n=nums.size();
-        int dp[n][2];
-        //0 up
-        // 1 down  
-        memset(dp,0,sizeof(dp));
-        dp[0][0]=1;
-        dp[0][1]=1;
+        int inc=1;
+        int dec=1;
         for(int i=1;i<n;i++)
         {
             if(nums[i-1]<nums[i])
             {
-                dp[i][0]=dp[i-1][1]+1;
-                dp[i][1]=dp[i-1][1];
+                inc=dec+1;
             }
             else if(nums[i-1]>nums[i])
             {
-                dp[i][0]=dp[i-1][0];
-                dp[i][1]=dp[i-1][0]+1;
-            }
-            else
-            {
-                dp[i][0]=dp[i-1][0];
-                dp[i][1]=dp[i-1][1];
+                dec=inc+1;
             }
         }
-        return max(dp[n-1][0],dp[n-1][1]);
+        return max(inc,dec);
     }
 };
