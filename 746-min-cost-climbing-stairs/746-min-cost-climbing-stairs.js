9e8eca4a87ebf76fe62a7f50a9c1b27973ad4405
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-10 18:12:45 +0530

    Time: 100 ms (44.07%), Space: 43 MB (67.87%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let n=cost.length;
+    let dp=new Array(n);
+    dp[0]=cost[0];
+    dp[1]=cost[1];
+    for(let i=2;i<n;i++){
+        dp[i]=Math.min(dp[i-1],dp[i-2])+cost[i];
+    }
+    return Math.min(dp[n-1],dp[n-2]);
+};
