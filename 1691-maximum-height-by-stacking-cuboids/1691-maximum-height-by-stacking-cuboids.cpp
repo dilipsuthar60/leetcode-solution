a919de54e70228a704756d8a56f63b3cfdeebe6b
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-19 16:53:33 +0530

    Time: 32 ms (23.94%), Space: 9 MB (87.79%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
-   int dp[110][110];
     int find(vector<vector<int>>&nums,int index,int prev)
     {
-        if(index==nums.size())
-        {
-            return 0;
-        }
-        if(dp[index][prev+1]!=-1)
-        {
-            return dp[index][prev+1];
-        }
-        int inc=0;
-        if(prev==-1||(nums[prev][0]<=nums[index][0]&&nums[prev][1]<=nums[index][1]&&nums[prev][2]<=nums[index][2]))
-        {
-            inc=nums[index][2]+find(nums,index+1,index);
-        }
-        int exc=find(nums,index+1,prev);
-        return dp[index][prev+1]=max(inc,exc);
+        return (nums[prev][0]<=nums[index][0]&&nums[prev][1]<=nums[index][1]&&nums[prev][2]<=nums[index][2]);
+        // if(index==nums.size())
+        // {
+        //     return 0;
+        // }
+        // if(dp[index][prev+1]!=-1)
+        // {
+        //     return dp[index][prev+1];
+        // }
+        // int inc=0;
+        // if(prev==-1||(nums[prev][0]<=nums[index][0]&&nums[prev][1]<=nums[index][1]&&nums[prev][2]<=nums[index][2]))
+        // {
+        //     inc=nums[index][2]+find(nums,index+1,index);
+        // }
+        // int exc=find(nums,index+1,prev);
+        // return dp[index][prev+1]=max(inc,exc);
     }
     int maxHeight(vector<vector<int>>&nums)
     {
-        memset(dp,-1,sizeof(dp));
+        int n=nums.size();
+        int dp[n];
+        memset(dp,0,sizeof(dp));
+        int index=0;
         for(auto &it:nums)
         {
             sort(it.begin(),it.end());
         }
         sort(nums.begin(),nums.end());
-        return find(nums,0,-1);
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            dp[i]=nums[i][2];
+            for(int j=0;j<i;j++)
+            {
+                if(find(nums,i,j)&&dp[j]+nums[i][2]>dp[i])
+                {
+                    dp[i]=dp[j]+nums[i][2];
+                }
+            }
+            ans=max(ans,dp[i]);
+        }
+        return ans;
     }
 };
