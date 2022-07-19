a29ea390f41712fc63d69a4ffdf700b0692a82dd
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-19 16:31:14 +0530

    Time: 30 ms (28.99%), Space: 9 MB (97.39%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    map<string,int>mp;
-    string get_hash(int a,int b,int c,int d)
-    {
-        return to_string(a)+"##"+to_string(b)+"##"+to_string(c)+"##"+to_string(d);
-    }
-    int find(vector<vector<int>>&nums,int index,int prev_w,int prev_l,int prev_h)
+   int dp[110][110];
+    int find(vector<vector<int>>&nums,int index,int prev)
     {
         if(index==nums.size())
         {
             return 0;
         }
+        if(dp[index][prev+1]!=-1)
+        {
+            return dp[index][prev+1];
+        }
         int inc=0;
-        string hash_value=get_hash(index,prev_w,prev_l,prev_h);
-        if(mp.find(hash_value)!=mp.end())
+        if(prev==-1||(nums[prev][0]<=nums[index][0]&&nums[prev][1]<=nums[index][1]&&nums[prev][2]<=nums[index][2]))
         {
-            return mp[hash_value];
+            inc=nums[index][2]+find(nums,index+1,index);
         }
-        if(prev_w<=nums[index][0]&&prev_l<=nums[index][1]&&prev_h<=nums[index][2])
-        {
-            inc=nums[index][2]+find(nums,index+1,nums[index][0],nums[index][1],nums[index][2]);
-        }
-        int exc=find(nums,index+1,prev_w,prev_l,prev_h);
-        return mp[hash_value]=max(inc,exc);
+        int exc=find(nums,index+1,prev);
+        return dp[index][prev+1]=max(inc,exc);
     }
     int maxHeight(vector<vector<int>>&nums)
     {
+        memset(dp,-1,sizeof(dp));
         for(auto &it:nums)
         {
             sort(it.begin(),it.end());
         }
         sort(nums.begin(),nums.end());
-        return find(nums,0,INT_MIN,INT_MIN,INT_MIN);
+        return find(nums,0,-1);
     }
 };
