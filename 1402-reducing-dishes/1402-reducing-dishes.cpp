9b7b1a2de7aca595eaf3a684af17aef263bea834
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-19 21:56:49 +0530

    Time: 4 ms (81.41%), Space: 8.1 MB (71.72%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     int n;
-    int dp[501][501];
-    int find(vector<int>&nums,int index,int time)
-    {
-        if(index>=n)
-        {
-            return 0;
-        }
-        if(dp[index][time]!=-1)
-        {
-            return dp[index][time];
-        }
-        int inc=nums[index]*(time+1)+find(nums,index+1,time+1);
-        int exc=find(nums,index+1,time);
-        return dp[index][time]=max(inc,exc);
-    }
+    // int dp[501][501];
+    // int find(vector<int>&nums,int index,int time)
+    // {
+    //     if(index>=n)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[index][time]!=-1)
+    //     {
+    //         return dp[index][time];
+    //     }
+    //     int inc=nums[index]*(time+1)+find(nums,index+1,time+1);
+    //     int exc=find(nums,index+1,time);
+    //     return dp[index][time]=max(inc,exc);
+    // }
     int maxSatisfaction(vector<int>&nums) 
     {
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         n=nums.size();
         sort(nums.begin(),nums.end());
-        return find(nums,0,0);
+        int ans=0;
+        int suffix=0;
+        int curr=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            suffix+=nums[i];
+            curr+=suffix;
+            ans=max(ans,curr);
+        }
+        return ans;
     }
 };
