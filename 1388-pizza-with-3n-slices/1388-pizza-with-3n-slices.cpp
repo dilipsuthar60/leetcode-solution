802f75ffc91dc7f0744791bee2d8070a4ba9f866
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-19 17:18:21 +0530

    Time: 18 ms (89.38%), Space: 8.7 MB (75.68%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int dp[510][310];
-    int find(vector<int>&nums,int index,int bound,int k)
+    int find(vector<int>&nums,int index,int bound,int k,int dp[][310])
     {
         if(index>=bound||k==0)
         {
@@ -11,18 +10,19 @@ public:
         {
             return dp[index][k];
         }
-        int inc=nums[index]+find(nums,index+2,bound,k-1);
-        int exc=find(nums,index+1,bound,k);
+        int inc=nums[index]+find(nums,index+2,bound,k-1,dp);
+        int exc=find(nums,index+1,bound,k,dp);
         return dp[index][k]=max(inc,exc);
     }
     int maxSizeSlices(vector<int>&nums) 
     {
+        int dp[510][310];
         int n=nums.size();
         int k=n/3;
         memset(dp,-1,sizeof(dp));
-        int val1=find(nums,0,n-1,k);
+        int val1=find(nums,0,n-1,k,dp);
         memset(dp,-1,sizeof(dp));
-        int val2=find(nums,1,n,k);
+        int val2=find(nums,1,n,k,dp);
         return max(val1,val2);
     }
 };
