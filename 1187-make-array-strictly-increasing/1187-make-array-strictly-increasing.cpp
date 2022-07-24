ba842e2a87908c1f03f2357049a8223a85bfa32e
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-24 22:33:54 +0530

    Time: 1261 ms (12.25%), Space: 75.6 MB (21.08%) - LeetHub

---
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int n;
+    int yes;
+    unordered_map<int,int>mp[2001];
+    int find(vector<int>&nums,int index,int prev,vector<int>&arr)
+    {
+        if(index==n)
+        {
+            yes=1;
+            return 0;
+        }
+        if(mp[index][prev]!=0)
+        {
+            return mp[index][prev];
+        }
+        int j=upper_bound(arr.begin(),arr.end(),prev)-arr.begin();
+        int ans=1e8;
+        if(prev<nums[index])
+        {
+            ans=min(ans,find(nums,index+1,nums[index],arr));
+        }
+        if(j<arr.size())
+        {
+            ans=min(ans,1+find(nums,index+1,arr[j],arr));
+        }
+        return mp[index][prev]=ans;
+        
+    }
+    int makeArrayIncreasing(vector<int>&nums, vector<int>& arr) 
+    {
+        yes=0;
+        n=nums.size();
+        sort(arr.begin(),arr.end()); 
+        int val=find(nums,0,-1,arr);
+        if(yes)
+        {
+            return val;
+        }
+        return -1;
+    }
+};
