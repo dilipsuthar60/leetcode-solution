9dd5e2961fb28a689e9da77e4920db2507e86b12
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-25 20:43:25 +0530

    Time: 85 ms (36.09%), Space: 14.9 MB (94.08%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+    public:
+        int n, m;
+    int dp[201][201];
+    int find(vector<vector < int>> &mat, int i, int j, int prev)
+    {
+        if (i < 0 || j < 0 || i >= n || j >= m || mat[i][j] <= prev) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = 1 + max({ find(mat, i - 1, j, mat[i][j]),
+            find(mat, i + 1, j, mat[i][j]),
+            find(mat, i, j - 1, mat[i][j]),
+            find(mat, i, j + 1, mat[i][j]) });
+    }
+    int longestIncreasingPath(vector<vector < int>> &mat)
+    {
+        n = mat.size();
+        m = mat[0].size();
+        int ans = 0;
+        memset(dp, -1, sizeof(dp));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                ans = max(ans, find(mat, i, j, -1));
+            }
+        }
+        return ans;
+    }
+};
