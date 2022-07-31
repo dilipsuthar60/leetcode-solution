988846896373a01ffea10fd16e03682e2498021c
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-07-31 22:18:35 +0530

    Time: 188 ms (100.00%), Space: 89.4 MB (100.00%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,63 +1,64 @@
-class Solution {
-public:
-    int longestCycle(vector<int>& nums) 
-    {
-        int n=nums.size();
-        int ind[n],vis[n];
-        memset(ind,0,sizeof(ind));
-        memset(vis,0,sizeof(vis));
-        for(int i=0;i<n;i++)
+class Solution
+{
+    public:
+        int longestCycle(vector<int> &nums)
         {
-            if(nums[i]==-1)
-                continue;
-            ind[nums[i]]++;
-        }
-        queue<int>q;
-        for(int i=0;i<n;i++)
-        {
-            if(ind[i]==0)
+            int n = nums.size();
+            int ind[n], vis[n];
+            memset(ind, 0, sizeof(ind));
+            memset(vis, 0, sizeof(vis));
+            for (int i = 0; i < n; i++)
             {
-                q.push(i);
+                if (nums[i] == -1)
+                    continue;
+                ind[nums[i]]++;
             }
-        }
-        while(q.size())
-        {
-            auto temp=q.front();
-            q.pop();
-            vis[temp]=1;
-            int child=nums[temp];
-            if(child==-1)
+            queue<int> q;
+            for (int i = 0; i < n; i++)
             {
-                continue;
+                if (ind[i] == 0)
+                {
+                    q.push(i);
+                }
             }
-            if(--ind[child]==0)
+            while (q.size())
             {
-                q.push(child);
+                auto temp = q.front();
+                q.pop();
+                vis[temp] = 1;
+                int child = nums[temp];
+                if (child == -1)
+                {
+                    continue;
+                }
+                if (--ind[child] == 0)
+                {
+                    q.push(child);
+                }
             }
-        }
-        int yes=0;
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            if(vis[i])
+            int yes = 0;
+            int ans = 0;
+            for (int i = 0; i < n; i++)
             {
-                continue;
+                if (vis[i])
+                {
+                    continue;
+                }
+                int count = 0;
+                int now = i;
+                while (vis[now] == 0)
+                {
+                    yes = 1;
+                    count++;
+                    vis[now] = 1;
+                    now = nums[now];
+                }
+                ans = max(ans, count);
             }
-            int count=0;
-            int now=i;
-            while(vis[now]==0)
+            if (yes == 0)
             {
-                yes=1;
-                count++;
-                vis[now]=1;
-                now=nums[now];
+                return -1;
             }
-            ans=max(ans,count);
+            return ans;
         }
-        if(yes==0)
-        {
-            return -1;
-        }
-        return ans;
-    }
 };
