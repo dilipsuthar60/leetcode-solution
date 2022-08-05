7e75fa3d1e4ca51ed70168a816f157bb1d15b0be
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-05 18:01:16 +0530

    Time: 62 ms (54.53%), Space: 13.8 MB (98.86%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -3,9 +3,12 @@ class Solution:
         n=len(nums)
         dp=[0]*(target+1)
         dp[0]=1
+        nums.sort()
         for i in range(1,target+1):
             for j in nums:
                 if i-j>=0:
-                    dp[i]+=dp[i-j]                    
+                    dp[i]+=dp[i-j]
+                else:
+                    break
         return dp[target]            
         
