3f072976b79711c0d16c6e7833b9bdb9edcf75db
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-07 00:10:15 +0530

    Time: 1591 ms (33.33%), Space: 34.2 MB (33.33%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -1,14 +1,13 @@
-class Solution {
-public:
-    long long countBadPairs(vector<int>& nums) {
-        unordered_map<long long ,long long>mp;
-        int n=nums.size();
-        long long count=1ll*n*(n-1)/2;
-        for(int i=0;i<n;i++)
-        {
-            count-=1ll*mp[nums[i]-i];
-            mp[nums[i]-i]++;
-        }
-        return count;
-    }
-};
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        mp=dict()
+        n=len(nums)
+        count=n*(n-1)//2
+        for i in range(0,n):
+            if nums[i]-i in mp:
+                count-=mp[nums[i]-i]
+            if nums[i]-i not in mp:
+                mp[nums[i]-i]=0
+            mp[nums[i]-i]+=1    
+        return count    
+        
