9860ecd69e16b91393e7f6ab00e04d2a40c74c5d
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-07 00:02:57 +0530

    Time: 454 ms (25.00%), Space: 88.6 MB (100.00%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -6,10 +6,7 @@ public:
         long long count=1ll*n*(n-1)/2;
         for(int i=0;i<n;i++)
         {
-            if(mp.find(nums[i]-i)!=mp.end())
-            {
-                count-=1ll*mp[nums[i]-i];
-            }
+            count-=1ll*mp[nums[i]-i];
             mp[nums[i]-i]++;
         }
         return count;
