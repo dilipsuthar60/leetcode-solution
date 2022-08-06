f5247d97d5959f7218d0996a0fe060de479cb9ca
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-07 00:05:48 +0530

    Time: 454 ms (25.00%), Space: 88.6 MB (100.00%) - LeetHub

---
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        unordered_map<long long ,long long>mp;
+        int n=nums.size();
+        long long count=1ll*n*(n-1)/2;
+        for(int i=0;i<n;i++)
+        {
+            count-=1ll*mp[nums[i]-i];
+            mp[nums[i]-i]++;
+        }
+        return count;
+    }
+};
