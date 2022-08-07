15efb57c4db1811ea4d8f0dbb22df7caa083c5e8
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-07 17:52:41 +0530

    Time: 889 ms (57.14%), Space: 46.7 MB (85.71%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
+    int n;
+    int dp[100005][27];
+    int find(string&s,int index,char prev,int &k)
+    {
+        if(index>=n)
+        {
+            return 0;
+        }
+        int prev_index=0;
+        if(prev=='#')
+        {
+            prev_index=26;
+        }
+        else
+        {
+            prev_index=int(prev-'a');
+        }
+        if(dp[index][prev_index]!=-1)
+        {
+            return dp[index][prev_index];
+        }
+        int ans=0;
+        if(prev=='#'||abs(prev-s[index])<=k)
+        {
+            ans=max(ans,1+find(s,index+1,s[index],k));
+        }
+        ans=max(ans,find(s,index+1,prev,k));
+        return dp[index][prev_index]= ans;
+    }
     int longestIdealString(string s, int k) 
     {
-        int n=s.size();
-        int dp[26];
-        memset(dp,0,sizeof(dp));
-        int ans=0;
-        for(auto &it:s)
-        {
-            int i=it-'a';
-            dp[i]=dp[i]+1;
-            for(int j=max(0,i-k);j<=min(25,i+k);j++)
-            {
-                if(i!=j)
-                dp[i]=max(dp[i],dp[j]+1);
-            }
-            ans=max(ans,dp[i]);
-        }
-        return ans;
+        memset(dp,-1,sizeof(dp));
+        n=s.size();
+        return find(s,0,'#',k);
     }
 };
