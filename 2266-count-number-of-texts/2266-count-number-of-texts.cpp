8135f1a08e64c9d51ce1c77b6f3449f081ecc1ad
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-07 23:51:19 +0530

    Time: 68 ms (72.83%), Space: 21 MB (35.11%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.cpp
+++ 2266-count-number-of-texts/2266-count-number-of-texts.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
+    int n;
     int mod=1e9+7;
-    int dp[100005];
+    long long dp[100006];
     int find(string &s,int index)
     {
-        if(index>=s.size())
+        if(index>=n)
         {
             return 1;
         }
         if(dp[index]!=-1)
         {
-            return dp[index];
+            return dp[index]%mod;
         }
-        long long ans=0;
-        ans+=find(s,index+1);
-        if((index+1<s.size())&&s[index]==s[index+1])
+        long long ans=find(s,index+1);
+        if(index+1<n&&s[index]==s[index+1])
         {
             ans+=find(s,index+2);
-            if(index+2<s.size()&&s[index]==s[index+2])
-            {
-                ans+=find(s,index+3);
-                if(index+3<s.size()&&s[index]==s[index+3]&&(s[index]=='7'||s[index]=='9'))
-                {
-                    ans+=find(s,index+4);
-                }
-            }
+        }
+        if(index+2<n&&s[index]==s[index+1]&&s[index]==s[index+2])
+        {
+            ans+=find(s,index+3);
+        }
+        if(index+3<n&&(s[index]=='7'||s[index]=='9')&&s[index]==s[index+1]&&s[index]==s[index+2]&&s[index]==s[index+3])
+        {
+            ans+=find(s,index+4);
         }
         return dp[index]=ans%mod;
     }
-    int countTexts(string s) 
-    {
+    int countTexts(string s) {
+        n=s.size();
         memset(dp,-1,sizeof(dp));
-        return find(s,0);
+        return find(s,0)%mod;
     }
 };
