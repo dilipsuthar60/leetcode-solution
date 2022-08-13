6fb886f01dfd4cbb2111830240238847e57bd1b0
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-13 20:48:42 +0530

    Time: 924 ms (33.94%), Space: 345.5 MB (14.21%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,78 @@
+// class Solution {
+// public:
+    
+//     bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
+//         for(int j=0; j<s.size(); j+=wordLen) {
+//             string w = s.substr(j, wordLen);
+//             if(wordCount.find(w) != wordCount.end()) {
+//                 if(--wordCount[w] == -1) {
+//                     return false;
+//                 }
+//             } else {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     vector<int> findSubstring(string s, vector<string>& words) {
+//         vector<int> res;
+//         int wordLen = words[0].size();
+//         int sLen = s.size();
+//         int wordsWindow = words.size() * wordLen;
+        
+//         unordered_map<string, int> wordCount;
+//         for(int i=0; i<words.size(); i++) {
+//             wordCount[words[i]]++;
+//         }
+        
+//         int i = 0;
+//         while(i + wordsWindow <= sLen) {
+//             if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
+//                 res.push_back(i);
+//             }
+//             i++;
+//         }
+//         return res;
+//     }
+// };
+class Solution {
+public:
+    int k;
+    bool find(string &s,int &window,unordered_map<string,int>mp)
+    {
+        if(s.size()<k)
+        {
+            return false;
+        }
+        for(int i=0;i<s.size();i+=window)
+        {
+            string str=s.substr(i,window);
+            if(--mp[str]==-1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<int> findSubstring(string s, vector<string>& words) 
+    {
+        unordered_map<string,int>mp;
+        int first_len=words[0].size();
+        k=words[0].size()*(words.size());
+        for(auto &it:words)
+        {
+            mp[it]++;
+        }
+        vector<int>ans;
+        for(int i=0;i<s.size()-k+1;i++)
+        {
+            string str=s.substr(i,k);
+            if(find(str,first_len,mp))
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
