df5900e72437b8dc92e8885c58ba17f6c62c468d
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-13 20:57:50 +0530

    Time: 526 ms (43.33%), Space: 160.8 MB (32.18%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,50 +1,8 @@
-// class Solution {
-// public:
-    
-//     bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
-//         for(int j=0; j<s.size(); j+=wordLen) {
-//             string w = s.substr(j, wordLen);
-//             if(wordCount.find(w) != wordCount.end()) {
-//                 if(--wordCount[w] == -1) {
-//                     return false;
-//                 }
-//             } else {
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-
-//     vector<int> findSubstring(string s, vector<string>& words) {
-//         vector<int> res;
-//         int wordLen = words[0].size();
-//         int sLen = s.size();
-//         int wordsWindow = words.size() * wordLen;
-        
-//         unordered_map<string, int> wordCount;
-//         for(int i=0; i<words.size(); i++) {
-//             wordCount[words[i]]++;
-//         }
-        
-//         int i = 0;
-//         while(i + wordsWindow <= sLen) {
-//             if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
-//                 res.push_back(i);
-//             }
-//             i++;
-//         }
-//         return res;
-//     }
-// };
 class Solution {
 public:
     int k;
     bool find(string &s,int &window,unordered_map<string,int>&mp)
     {
-        if(s.size()<k)
-        {
-            return false;
-        }
         for(int i=0;i<s.size();i+=window)
         {
             string str=s.substr(i,window);
