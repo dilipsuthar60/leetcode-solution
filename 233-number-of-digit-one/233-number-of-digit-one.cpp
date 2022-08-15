027182d517830155c4b723f8673d827d5602b796
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-15 18:30:35 +0530

    Time: 0 ms (100.00%), Space: 6 MB (33.04%) - LeetHub

---
+++ 233-number-of-digit-one/233-number-of-digit-one.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string s;
+    int dp[10][2][20];
+    int find(int n,int pos=0,int tight=1,int count=0)
+    {
+        if(pos==n)
+        {
+            return count;
+        }
+        if(dp[pos][tight][count]!=-1)
+        {
+            return dp[pos][tight][count];
+        }
+        int ans=0;
+        if(tight==1)
+        {
+            for(int i=0;i<=s[pos]-'0';i++)
+            {
+                int new_tight=0;
+                int new_count=count;
+                if(i==s[pos]-'0')
+                {
+                    new_tight=1;
+                }
+                if(i==1)
+                {
+                    new_count++;
+                }
+                ans+=find(n,pos+1,new_tight,new_count);
+            }
+        }
+        else
+        {
+            for(int i=0;i<=9;i++)
+            {
+                int new_count=count;
+                if(i==1)
+                {
+                    new_count++;
+                }
+                ans+=find(n,pos+1,0,new_count);
+            }
+        }
+        return dp[pos][tight][count]=ans;
+    }
+    int countDigitOne(int n) 
+    {
+        memset(dp,-1,sizeof(dp));
+        s=to_string(n);
+        return find(s.size());
+    }
+};
