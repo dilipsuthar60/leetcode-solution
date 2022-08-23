7bd3d11d7725d251797881ceb1611417976f7626
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-23 18:20:52 +0530

    Time: 697 ms (22.57%), Space: 112.5 MB (19.67%) - LeetHub

--- 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.cpp
+++ 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.cpp
@@ -1,44 +1,74 @@
 class Solution {
 public:
-    int find(int mask,vector<int>&nums,int &arrow,vector<int>&temp)
+    // int find(int mask,vector<int>&nums,int &arrow,vector<int>&temp)
+    // {
+    //     int score=0;
+    //     for(int i=0;i<12;i++)
+    //     {
+    //         if(mask&(1<<i))
+    //         {
+    //             int need=nums[i]+1;
+    //             if(need>arrow)
+    //             {
+    //                 return -1;
+    //             }
+    //             temp[i]=need;
+    //             score+=i;
+    //             arrow-=need;
+    //         }
+    //     }
+    //     return score;
+    // }
+    vector<int>ans;
+    int max_score=0;
+    void find(int index,int arrow,int usearrow,int score,vector<int>&alice,vector<int>temp)
     {
-        int score=0;
-        for(int i=0;i<12;i++)
+        // cout<<usearrow<<"  "<<score<<endl;
+        if(index==12)
         {
-            if(mask&(1<<i))
-            {
-                int need=nums[i]+1;
-                if(need>arrow)
-                {
-                    return -1;
-                }
-                temp[i]=need;
-                score+=i;
-                arrow-=need;
-            }
-        }
-        return score;
-    }
-    vector<int> maximumBobPoints(int num, vector<int>& alice) 
-    {    
-        vector<int>ans(12,0);
-        vector<int>temp(12,0);
-        int max_score=0;
-        for(int i=0;i<(1<<12);i++)
-        {
-            int usearrow=num;
-            int score=find(i,alice,usearrow,temp);
-            if(score!=-1&&max_score<score)
+            if(max_score<score)
             {
                 max_score=score;
                 ans=temp;
                 if(usearrow)
                 {
-                    ans[0]+=usearrow;
+                    ans.back()+=usearrow;
                 }
             }
-            temp=vector<int>(12,0);
+            return ;
         }
+        int need=alice[index]+1;
+        if(usearrow>=need)
+        {
+            temp[index]=need;
+            find(index+1,arrow,usearrow-need,score+index,alice,temp);
+            temp[index]=0;
+        }
+        find(index+1,arrow,usearrow,score,alice,temp);
+    }
+    vector<int> maximumBobPoints(int num, vector<int>& alice) 
+    {    
+        vector<int>temp(12);
+        find(0,num,num,0,alice,temp);
         return ans;
+        // vector<int>ans(12,0);
+        // vector<int>temp(12,0);
+        // int max_score=0;
+        // for(int i=0;i<(1<<12);i++)
+        // {
+        //     int usearrow=num;
+        //     int score=find(i,alice,usearrow,temp);
+        //     if(score!=-1&&max_score<score)
+        //     {
+        //         max_score=score;
+        //         ans=temp;
+        //         if(usearrow)
+        //         {
+        //             ans[0]+=usearrow;
+        //         }
+        //     }
+        //     temp=vector<int>(12,0);
+        // }
+        // return ans;
     }
 };
