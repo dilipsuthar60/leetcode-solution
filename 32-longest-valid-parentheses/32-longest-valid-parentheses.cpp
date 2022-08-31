75956f8f19855dc7caf78d865e621386d6a62cbe
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-08-31 23:17:37 +0530

    Time: 11 ms (24.24%), Space: 7.8 MB (9.44%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -4,17 +4,20 @@ public:
         vector<int>stack;
         int n=s.size();
         vector<int>nums(n,0);
+        int stack_count=0;
         for(int i=0;i<n;i++)
         {
             if(s[i]=='(')
             {
+                stack_count++;
                 stack.push_back(i);
             }
-            else if(stack.size())
+            else if(stack_count)
             {
                 nums[i]=1;
                 nums[stack.back()]=1;
                 stack.pop_back();
+                stack_count--;
             }
         }
         int count=0;
