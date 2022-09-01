7d54793d22ed409ae28a8a3456a2de74d52f9032
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-09-01 21:44:01 +0530

    Time: 755 ms (34.04%), Space: 14 MB (87.12%) - LeetHub

--- 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
+++ 2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.cpp
@@ -1,52 +1,127 @@
+//  int ans=1e9,xo=0,time=0;
+//     void dfs(int node, int par, vector<vector<int>>& adj, vector<int>& nums, vector<int>& sub, vector<int>& in, vector<int>& out)
+//     {
+//         in[node]=time++;
+//         sub[node]=nums[node];
+//         for(auto child: adj[node])
+//         {
+//             if(child==par) continue;
+//             dfs(child,node,adj,nums,sub,in,out);
+//             sub[node]^=sub[child];
+//         }
+//         out[node]=time++;
+//     }
+    
+//     int minimumScore(vector<int>& nums, vector<vector<int>>& edges)
+//     {
+//         int n=nums.size();
+//         vector<vector<int>> adj(n);
+//         for(int i=0;i<n;i++)
+//         {
+//             xo^=nums[i];
+//         }
+//         for(int i=0;i<n-1;i++)
+//         {
+//             adj[edges[i][0]].push_back(edges[i][1]);
+//             adj[edges[i][1]].push_back(edges[i][0]);
+//         }
+//         vector<int> sub(n,0),in(n),out(n);
+//         dfs(0,0,adj,nums,sub,in,out);
+//         for(int i=1;i<n;i++)
+//         {
+//             for(int j=1;j<n;j++)
+//             {
+//                 if(i==j) continue;
+//                 else if(in[i]<in[j] && out[i]>out[j])
+//                 {
+//                     int x=sub[j];
+//                     int y=sub[i]^sub[j];
+//                     int z=xo^sub[i];
+//                     int mn=min(x,min(y,z)),mx=max(x,max(y,z));
+//                     ans=min(ans,mx-mn);
+//                 }
+//                 else if(in[i]>in[j] && out[i]<out[j])
+//                 {
+//                     int x=sub[i];
+//                     int y=sub[i]^sub[j];
+//                     int z=xo^sub[j];
+//                     int mn=min(x,min(y,z)),mx=max(x,max(y,z));
+//                     ans=min(ans,mx-mn);
+//                 }
+//                 else 
+//                 {
+//                     int x=sub[i];
+//                     int y=sub[j];
+//                     int z=xo^sub[i]^sub[j];
+//                     int mn=min(x,min(y,z)),mx=max(x,max(y,z));
+//                     ans=min(ans,mx-mn);
+//                 }
+//             }
+//         }
+//         return ans;
 class Solution {
 public:
-    vector<int>nums;
-    unordered_set<int>dp[1001];
-    int ans=1e9;
-    int find(int x,int p)
+    int in[1001],out[1001];
+    int sub[1001];
+    int time=1;
+    vector<int>dp[1001];
+    void find(int node,vector<int>&nums,int p=-1)
     {
-        int ans=nums[x];
-        for(auto &it:dp[x])
+        in[node]=time++;
+        sub[node]=nums[node];
+        for(auto &it:dp[node])
         {
             if(it!=p)
             {
-                ans^=find(it,x);
+               find(it,nums,node);
+               sub[node]^=sub[it];
             }
         }
-        return ans;
+        out[node]=time++;
     }
-    int find2(int x,int p,int &comp1,int &comp2)
+    int minimumScore(vector<int>& nums, vector<vector<int>>& edges) 
     {
-        int curr=nums[x];
-        for(auto &it:dp[x])
+        int n=nums.size();
+        for(auto it:edges)
         {
-            if(it!=p)
+            dp[it[0]].push_back(it[1]);
+            dp[it[1]].push_back(it[0]);
+        }
+        find(0,nums);
+        int total=sub[0];
+        int ans=1e9;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<n;j++)
             {
-                int val=find2(it,x,comp1,comp2);
-                ans=min(ans,max({val,comp1^val,comp2})-min({val,comp1^val,comp2}));
-                curr^=val;
+                if(i==j)
+                {
+                    continue;
+                }
+                int x=i;
+                int y=j;
+                if(in[x]<in[y]&&out[x]>out[y])
+                {
+                    int first=sub[y];
+                    int second=sub[x]^sub[y];
+                    int third=total^sub[x];
+                    ans=min({ans,max({first,second,third})-min({first,second,third})});
+                }
+                else if(in[x]>in[y]&&out[x]<out[y])
+                {
+                     int first=sub[x];
+                     int second=sub[y]^sub[x];
+                     int third=total^sub[y];
+                      ans=min({ans,max({first,second,third})-min({first,second,third})});
+                }
+                else
+                {
+                    int first=sub[x];
+                    int second=sub[y];
+                    int third=total^sub[x]^sub[y];
+                    ans=min({ans,max({first,second,third})-min({first,second,third})});
+                }
             }
-        }
-        return curr;
-    }
-    int minimumScore(vector<int>&v, vector<vector<int>>& edges) 
-    {
-        nums=v;
-        for(auto &it:edges)
-        {
-            dp[it[0]].insert(it[1]);
-            dp[it[1]].insert(it[0]);
-        }
-        for(auto &it:edges)
-        {
-            dp[it[0]].erase(it[1]);
-            dp[it[1]].erase(it[0]);
-            int sum1=find(it[0],-1);
-            int sum2=find(it[1],-1);
-            find2(it[0],-1,sum1,sum2);
-            find2(it[1],-1,sum2,sum1);
-            dp[it[0]].insert(it[1]);
-            dp[it[1]].insert(it[0]);
         }
         return ans;
     }
