31fb269a132f467ffa7fa424ce04cc38a5c0f9b0
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-09-07 21:37:55 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (93.18%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    int dp[110];
+    int n;
+    int dp[101];
     int find(string &s,int index)
-   {
-        if(index==s.size())
+    {
+        if(index>=n)
         {
             return 1;
         }
@@ -16,39 +17,19 @@ public:
             return dp[index];
         }
         int ans=find(s,index+1);
-        if(index+1<s.size()&&stoi(s.substr(index,2))>=10&&stoi(s.substr(index,2))<=26)
+        if(index+1<n)
         {
-            ans+=find(s,index+2);
+            int val=stoi(s.substr(index,2));
+            if(val>=10&&val<=26)
+            {
+                ans+=find(s,index+2);
+            }
         }
         return dp[index]=ans;
     }
-    int numDecodings(string s) 
-    {
+    int numDecodings(string s) {
+        n=s.size();
         memset(dp,-1,sizeof(dp));
         return find(s,0);
-//         if(s.size()==1&&s[0]=='0')
-//         {
-//             return 0;
-//         }
-
-//          int n=s.size();
-//         vector<int>dp(n+1,0);
-//         dp[0]=(s[0]=='0'?0:1);
-//         dp[1]=(s[1]=='0'?0:1);
-//         for(int i=2;i<=n;i++)
-//         {
-//             int first=stoi(s.substr(i-1,1));
-//             int second=stoi(s.substr(i-2,2));
-//             if(first>=1&&first<=9)
-//             {
-//                 dp[i]+=dp[i-1];
-//             }
-//             if(second>=10&&second<=26)
-//             {
-//                 dp[i]+=dp[i-2];
-//             }
-//         }
-        
-//         return dp[0]==0?0:dp[n];
     }
 };
