a828c288f5091242f5fe66464fd9f0bfa87973e6
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-09-18 10:51:59 +0530

    Time: 2637 ms (12.50%), Space: 134.6 MB (62.50%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
@@ -1,53 +1,29 @@
 class Solution {
 public:
-        struct node
-    {
-        node*child[26]={NULL};
-        int count=0;
-    };
-    node*root=new node();
-    void insert(string &s)
-    {
-        int n=s.size();
-        node*curr=root;
-        for(int i=0;i<n;i++)
-        {
-            int index=s[i]-'a';
-            if(curr->child[index]==NULL)
-            {
-                curr->child[index]=new node();
-            }
-            curr=curr->child[index];
-            curr->count++;
-        }
-    }
-    int find(node*curr,string &s)
-    {
-        int ans=0;
-        for(char &ch:s)
-        {
-            if(curr->child[ch-'a']!=NULL)
-            {
-                curr=curr->child[ch-'a'];
-                ans+=curr->count;
-            }
-        }
-        return ans;
-    }
     vector<int> sumPrefixScores(vector<string>&nums) 
     {
         int n=nums.size();
+        unordered_map<unsigned long long,int>mp;
         for(int i=0;i<n;i++)
         {
-            insert(nums[i]);
+            unsigned long long hash=0;
+            for(auto &ch:nums[i])
+            {
+                hash=hash*31+(ch);
+                mp[hash]++;
+            }
         }
-        vector<int>ans;
+        vector<int>ans(n,0);
         for(int i=0;i<n;i++)
         {
-            node*curr=root;
-            int value=find(curr,nums[i]);
-            // cout<<endl;
-            ans.push_back(value);
+            int count=0;
+            unsigned long long hash=0;
+            for(auto &ch:nums[i])
+            {
+                hash=hash*31+(ch);
+                count+=mp[hash];
+            }
+            ans[i]=count;
         }
         return ans;
     }
