14411faaf5c12e9f5e18ae7aab2bb655e43b6687
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-09-18 21:53:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (50.00%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        if(n&1)
-        {
-            return n*2;
-        }
-        return  n;
+       return n&1?n*2:n;
     }
 };
