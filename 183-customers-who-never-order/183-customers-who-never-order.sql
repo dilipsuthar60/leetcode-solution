0c637c097a6cd1c06ce19e9c4b5cf67dcd338dbb
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-09-19 20:07:17 +0530

    Time: 607 ms (45.30%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,1 +1,1 @@
-select customers.name as customers from customers where customers.id not in (select orders.customerid from orders);
+select name as customers from customers where id not in (select customerid from orders)
