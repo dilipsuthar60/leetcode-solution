5c30ab3a51e2e2f4c1125b0f5e82836cd82b6759
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-09-19 16:40:39 +0530

    Time: 421 ms (46.00%), Space: 126.8 MB (32.01%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.cpp
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -1,58 +1,50 @@
 class Solution {
 public:
-    struct node
+    unordered_set<string>st;
+    bool find(string s)
     {
-        node*child[26]={NULL};
-        int end=0;
-    };
-    node*root=new node();
-    void insert(string s)
-    {
+        queue<int>q;
+        q.push(0);
         int n=s.size();
-        node*curr=root;
-        for(int i=0;i<n;i++)
+        int vis[301];
+        memset(vis,0,sizeof(vis));
+        while(q.size())
         {
-            if(curr->child[s[i]-'a']==NULL)
-            {
-                curr->child[s[i]-'a']=new node();
-            }
-            curr=curr->child[s[i]-'a'];
-        }
-        curr->end=1;
-    }
-    bool find(node* root,node* curr,int index,string &s,int count)
-    {
-        if(!curr)
-        {
-            return false;
-        }
-        if(index>=s.size())
-        {
-            if(curr->end&&count>=1)
+            auto temp=q.front();
+            q.pop();
+            if(temp==n)
             {
                 return true;
             }
-            return false;
-        }
-        if(curr->end&&find(root,root,index,s,count+1))
-        {
-            return true;
-        }
-        return find(root,curr->child[s[index]-'a'],index+1,s,count);
-    }
-    vector<string> findAllConcatenatedWordsInADict(vector<string>& nums) 
-    {
-        vector<string>ans;
-        sort(nums.begin(),nums.end(),[&](auto &a,auto &b){return a.size()<b.size();});
-        for(auto &s:nums)
-        {
-            if(find(root,root,0,s,0))
+            if(vis[temp])
             {
-                ans.push_back(s);
+                continue;
+            }
+            vis[temp]=1;
+            string str="";
+            for(int i=temp;i<n;i++)
+            {
+                str.push_back(s[i]);
+                if(st.find(str)!=st.end())
+                {
+                    q.push(i+1);
+                }
+            }
+        }
+        return false;
+    }
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        sort(words.begin(),words.end(),[&](auto &a,auto &b){return a.size()<b.size();});
+        vector<string>ans;
+        for(auto it:words)
+        {
+            if(find(it))
+            {
+                ans.push_back(it);
             }
             else
             {
-                insert(s);
+                st.insert(it);
             }
         }
         return ans;
