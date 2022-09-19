e20a57a5a18c456c971b9b727eae97857670e32f
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-09-19 20:50:46 +0530

    Time: 1009 ms (5.00%), Space: 0B (100.00%) - LeetHub

---
+++ 627-swap-salary/627-swap-salary.sql
@@ -0,0 +1,15 @@
+update salary set sex=
+case 
+when sex='m' then 'f'
+when sex='f' then 'm'
+end
+
+
+
+
+# UPDATE Salary
+# SET sex = 
+# CASE
+#     WHEN sex = "f" THEN "m"
+#     WHEN sex = "m" THEN "f"
+# END
