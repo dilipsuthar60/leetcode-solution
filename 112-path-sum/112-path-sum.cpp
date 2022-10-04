f06e93a6e94a093a15b577ebbdad81718be614f1
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-10-04 21:47:55 +0530

    Time: 10 ms (90.94%), Space: 21.3 MB (74.14%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
-    class Node
+    bool find(TreeNode*root,int sum)
     {
-        public:
-        TreeNode* node;
-        int sum;
-        Node(TreeNode* node,int sum)
-        {
-            this->node=node;
-            this->sum=sum;
-        }
-    };
-    bool hasPathSum(TreeNode* root, int targetSum) {
         if(root==NULL)
         {
-            return 0;
+            return false;
         }
-        queue<Node>q;
-        Node curr(root,root->val);
-        q.push(curr);
-        while(q.size())
+        if(root->left==NULL&&root->right==NULL)
         {
-            Node curr=q.front();
-            q.pop();
-            if(curr.node->left==NULL&&curr.node->right==NULL)
-            {
-                if(curr.sum==targetSum)
-                {
-                    return true;
-                }
-            }
-            if(curr.node->left)
-            {
-                q.push(Node(curr.node->left,curr.sum+curr.node->left->val));
-            }
-            if(curr.node->right)
-            {
-                q.push(Node(curr.node->right,curr.sum+curr.node->right->val));
-            }
+            return sum-root->val==0;
+        }
+        if(find(root->left,sum-root->val)||find(root->right,sum-root->val))
+        {
+            return true;
         }
         return false;
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return find(root,targetSum);
     }
 };
