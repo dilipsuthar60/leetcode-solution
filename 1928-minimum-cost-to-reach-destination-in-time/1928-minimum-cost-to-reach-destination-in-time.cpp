5805625b4d8c00e61ae799a27256b59c7f12ed59
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-10-05 20:17:36 +0530

    Time: 247 ms (86.27%), Space: 26.3 MB (87.43%) - LeetHub

---
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    int minCost(int maxtime,vector<vector<int>>&nums,vector<int>&fee) 
+    {
+        int n=fee.size();
+        vector<pair<int,int>>dp[n+100];
+        vector<int>dis(n+1000,INT_MAX);
+        for(auto it:nums)
+        {
+            dp[it[0]].push_back({it[1],it[2]});
+            dp[it[1]].push_back({it[0],it[2]});
+        }
+        priority_queue<array<int,3>,vector<array<int,3>>,greater<array<int,3>>>pq;
+        pq.push({fee[0],0,0});
+        while(pq.size())
+        {
+            auto temp=pq.top();
+            pq.pop();
+            int f=temp[0];
+            int time=temp[1];
+            int node=temp[2];
+            if(node==n-1)
+            {
+                return f;
+            }
+            for(auto &it:dp[node])
+            {
+                int new_time=time+it.second;
+                if(new_time<=maxtime&&dis[it.first]>(time+it.second))
+                {
+                   dis[it.first]=new_time;
+                   pq.push({f+fee[it.first],new_time,it.first});
+                }
+            }
+        }
+        return -1;
+    }
+};
