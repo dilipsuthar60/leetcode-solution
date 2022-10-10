03b45399e6e889340fcb3daf0581c0ec3c375f4b
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-10-10 22:16:57 +0530

    Time: 1703 ms (5.01%), Space: 149.9 MB (8.26%) - LeetHub

--- 2412-minimum-money-required-before-transactions/2412-minimum-money-required-before-transactions.cpp
+++ 2412-minimum-money-required-before-transactions/2412-minimum-money-required-before-transactions.cpp
@@ -38,7 +38,7 @@ public:
         sort(p.begin(),p.end(),[&](auto &a,auto &b){return a[0]>b[0];});
         sort(l.begin(),l.end(),[&](auto &a,auto &b){return (a[1]==b[1])?a[0]>b[0]:a[1]<b[1];});
         long long x=0;
-        long long y=1e18;
+        long long y=1e15;
         long long ans=0;
         while(x<=y)
         {
@@ -56,90 +56,3 @@ public:
         return ans;
     }
 };
-//     static bool cmp2(vector<int> &a,vector<int> &b)
-//     {
-//        if (a[1]==b[1])
-//            return a[0]>b[0];
-        
-//         return a[1]<b[1];
-//     }
-    
-    
-//      static bool cmp1(vector<int> &a,vector<int> &b)
-//     {
-//         return a[0]>b[0];
-//     }
-    
-//     long long minimumMoney(vector<vector<int>>& transactions) {
-        
-//         long long low=0,high=0;
-//         for (auto x:transactions)
-//         {
-//           high+=x[0];
-//         }
-        
-//         vector<vector<int>> profit,loss;
-        
-//         for (auto x:transactions)
-//         {
-//             if (x[1]>=x[0])
-//               profit.push_back({x[0],x[1]});
-//             else
-//               loss.push_back({x[0],x[1]});  
-//         }
-        
-        
-//         sort(profit.begin(),profit.end(),cmp1);
-//         sort(loss.begin(),loss.end(),cmp2);
-        
-        
-        
-//         long long ans;
-        
-        
-        
-//         // for (auto x:transactions)
-//         // {
-//         //     cout<<x[0]<<" "<<x[1]<<endl;
-//         // }
-        
-//         while (low<=high)
-//         {
-//             long long mid=high+(low-high)/2;
-            
-//             if (isPossible(mid,profit,loss))
-//             {
-//                 ans=mid;
-//                 high=mid-1;
-//             }
-//             else
-//             {
-//                 low=mid+1;
-//             }
-            
-//         }
-        
-//         return ans;
-        
-//     }
-    
-//     bool isPossible(long long price,vector<vector<int>> &profit,vector<vector<int>> &loss)
-//     {
-//         for (int i=0;i<loss.size();++i)
-//         {
-//             if (price<loss[i][0])
-//                 return false;
-            
-//             price-=(loss[i][0]-loss[i][1]);
-//         }
-        
-//         for (int i=0;i<profit.size();++i)
-//         {
-//             if (price<profit[i][0])
-//                 return false;
-            
-//             price-=(profit[i][0]-profit[i][1]);
-//         }
-        
-//         return true;
-//     }
