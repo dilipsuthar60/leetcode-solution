e810e14da32128f3f7e5fd3f756efd71163cd87e
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-10-20 19:42:25 +0530

    Time: 420 ms (9.56%), Space: 90.8 MB (24.15%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int find(vector<int>&nums1,vector<int>&nums2)
+    int find(vector<int>&nums)
     {
-        int sum=accumulate(nums1.begin(),nums1.end(),0);
-        int s=0;
-        int a=0;
-        int n=nums1.size();
-        for(int i=0;i<n;i++)
+        int sum=0;
+        int max_sum=0;
+        for(int i=0;i<nums.size();i++)
         {
-            s=max(nums2[i]-nums1[i],s+nums2[i]-nums1[i]);
-            a=max(s,a);
+            sum=max(nums[i],nums[i]+sum);
+            max_sum=max(max_sum,sum);
         }
-        return sum+a;
+        return max_sum;
     }
     int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) 
     {
-        return max(find(nums1,nums2),find(nums2,nums1));
+        int sum1=accumulate(nums1.begin(),nums1.end(),0);
+        int sum2=accumulate(nums2.begin(),nums2.end(),0);
+        vector<int>first,second;
+        for(int i=0;i<nums1.size();i++)
+        {
+            first.push_back(nums2[i]-nums1[i]);
+        }
+        for(int i=0;i<nums2.size();i++)
+        {
+            second.push_back(nums1[i]-nums2[i]);
+        }
+        return max({sum1+find(first),sum2+find(second)});
     }
 };
