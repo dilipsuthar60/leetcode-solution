0b6db29f1abfc9278f85ce998bb79eca97600932
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-10-21 14:50:54 +0530

    Added solution - LeetHub

---
+++ IPL 2021 - Match Day 4 - GFG/ipl-2021-match-day-4.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+ 
+class Solution
+{
+    public:
+    // Function for finding maximum AND value.
+    int find(int val,int arr[],int n)
+    {
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if((arr[i]&val)==val)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    int maxAND (int arr[], int n)
+    {
+        int ans=0;
+        for(int i=31;i>=0;i--)
+        {
+            int count=find(ans|(1<<i),arr,n);
+            if(count>=2)
+            {
+                ans|=(1<<i);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+ 
+// Driver function
+int main()
+{
+    int t;
+    cin>>t;//testcases
+    while(t--)
+    {
+        int n;
+        cin>>n;//input n
+        int arr[n+5],i;
+        
+        //inserting elements
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        Solution obj;
+        //calling maxAND() function
+        cout <<  obj.maxAND(arr,n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
