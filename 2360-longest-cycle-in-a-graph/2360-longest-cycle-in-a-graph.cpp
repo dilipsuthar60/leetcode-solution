a72f74b359d1fd4232b88fc8b9a2dc85d27ff9dc
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-10-27 14:45:00 +0530

    Time: 846 ms (31.55%), Space: 180.7 MB (28.13%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,56 +1,101 @@
 class Solution {
 public:
-    int yes;
-    int n;
     vector<vector<int>>dp;
-    vector<int>depth;
-    vector<int>vis;
-    int ans;
-    void find(int x,int d)
+    void find(int node,vector<int>&vis,int &maxCycle,vector<int>&depth,int d=0)
     {
-        depth[x]=d;
-        vis[x]=1;
-        for(auto &it:dp[x])
+        vis[node]=1;
+        depth[node]=d;
+        for(auto &it:dp[node])
         {
             if(vis[it]==0)
             {
-                find(it,d+1);
+                find(it,vis,maxCycle,depth,d+1);
             }
             else if(vis[it]==1)
             {
-                cout<<it<<" "<<x<<endl;
-                yes=1;
-                ans=max(ans,depth[x]-depth[it]+1);
+                // cout<<depth[it]<<"  "<<depth[node]<<endl;
+                maxCycle=max(maxCycle,depth[node]-depth[it]+1);
             }
         }
-        vis[x]=2;
+        vis[node]=2;
     }
     int longestCycle(vector<int>&nums) 
     {
-        yes=0;
-        n=nums.size();
+        int n=nums.size();
         dp=vector<vector<int>>(n+1);
-        depth=vector<int>(n+1,-1);
-        vis=vector<int>(n+1);
         for(int i=0;i<n;i++)
         {
             if(nums[i]!=-1)
             {
-                // dp[i].push_back(nums[i]);
-                  dp[nums[i]].push_back(i);
+                dp[i].push_back(nums[i]);
             }
         }
+        int maxCycle=-1;
+        vector<int>vis(n+1,0),depth(n+1,0);
         for(int i=0;i<n;i++)
         {
             if(vis[i]==0)
             {
-                find(i,0);
+                find(i,vis,maxCycle,depth);
             }
         }
-        if(yes)
-        {
-            return ans;
-        }
-        return -1;
+        return maxCycle;
     }
 };
+// class Solution {
+// public:
+//     int yes;
+//     int n;
+//     vector<vector<int>>dp;
+//     vector<int>depth;
+//     vector<int>vis;
+//     int ans;
+//     void find(int x,int d)
+//     {
+//         depth[x]=d;
+//         vis[x]=1;
+//         for(auto &it:dp[x])
+//         {
+//             if(vis[it]==0)
+//             {
+// 			//forward egde
+//                 find(it,d+1);
+//             }
+//             else if(vis[it]==1)
+//             {
+// 			// back edge
+//                 yes=1;
+//                 ans=max(ans,depth[x]-depth[it]+1);
+//             }
+//         }
+// 		// visited this path
+//         vis[x]=2; 
+//     }
+//     int longestCycle(vector<int>&nums) 
+//     {
+//         yes=0;
+//         n=nums.size();
+//         dp=vector<vector<int>>(n+1);
+//         depth=vector<int>(n+1,-1);
+//         vis=vector<int>(n+1);
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]!=-1)
+//             {
+//                 dp[i].push_back(nums[i]);
+//             }
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//             if(vis[i]==0)
+//             {
+//                 find(i,0);
+//             }
+//         }
+//         if(yes)
+//         {
+//             return ans;
+//         }
+//         return -1;
+//     }
+// };
