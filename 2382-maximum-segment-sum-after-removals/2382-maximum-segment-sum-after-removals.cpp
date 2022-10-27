2abb9275e5c414895697b4de09f7783eae546d6b
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-10-27 14:27:42 +0530

    Time: 271 ms (98.42%), Space: 116.2 MB (61.79%) - LeetHub

--- 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.cpp
+++ 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.cpp
@@ -1,56 +1,54 @@
 class Solution {
 public:
-    int n;
-    long long max_val=0;
-    vector<long long>parent,sum;
-    int find(int x)
+    int find(int x,vector<long long>&parent)
     {
         if(x==parent[x])
         {
             return x;
         }
-        return parent[x]=find(parent[x]);
+        return parent[x]=find(parent[x],parent);
     }
-    void merge(int x,int y)
+    vector<long long> maximumSegmentSum(vector<int>& nums, vector<int>& q) 
     {
-        x=find(x);
-        y=find(y);
-        if(x==y)
-        {
-            return ;
-        }
-        sum[y]+=sum[x];
-        max_val=max(max_val,sum[y]);
-        parent[x]=y;
-    }
-    vector<long long> maximumSegmentSum(vector<int>& nums, vector<int>& r)
-    {
-        n=nums.size();
-        parent.resize(n+10);
-        sum.resize(n+10);
+        int n=nums.size();
+        vector<long long>parent(n),sum(n),vis(n,0);
+        iota(parent.begin(),parent.end(),0);
         for(int i=0;i<n;i++)
         {
             sum[i]=nums[i];
-            parent[i]=i;
         }
-        vector<int>vis(n+1,0);
-        vector<long long>ans(n,0);
-         max_val=0;
+        vector<long long>ans;
+        long long max_val=0;
         for(int i=n-1;i>=0;i--)
         {
-            ans[i]=max_val;
-            int index=r[i];
+            ans.push_back(max_val);
+            int index=q[i];
             vis[index]=1;
             max_val=max(max_val,1ll*nums[index]);
-            if(index+1<n&&vis[index+1])
-            {
-                merge(index,index+1);
-            }
             if(index-1>=0&&vis[index-1])
             {
-                merge(index,index-1);
+                int first=find(index,parent);
+                int second=find(index-1,parent);
+                if(first!=second)
+                {
+                    sum[first]+=sum[second];
+                    parent[second]=first;
+                    max_val=max(max_val,sum[first]);
+                }
+            }
+            if(index+1<n&&vis[index+1])
+            {
+                int first=find(index,parent);
+                int second=find(index+1,parent);
+                if(first!=second)
+                {
+                    sum[first]+=sum[second];
+                    parent[second]=first;
+                    max_val=max(max_val,sum[first]);
+                }
             }
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
