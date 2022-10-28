71efaabf71d3fb450717a70caa3903c0d3cd946a
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-10-28 08:56:31 +0530

    Time: 330 ms (5.03%), Space: 26.3 MB (13.62%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string hash(string &s)
+    {
+        int dp[26];
+        memset(dp,0,sizeof(dp));
+        string hash_value="";
+        for(int i=0;i<s.size();i++)
+        {
+            dp[s[i]-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            hash_value+=to_string(dp[i])+"##";
+        }
+        return hash_value;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>mp;
+        for(auto it:strs)
+        {
+            mp[hash(it)].push_back(it);
+        }
+        for(auto&[a,b]:mp)
+        {
+            ans.push_back(b);
+        }
+        return ans;
+    }
+};
