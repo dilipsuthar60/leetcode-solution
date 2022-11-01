80c2a4b91a14f78e979ce7618848e84dd970367e
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-11-01 19:32:28 +0530

    Time: 835 ms (5.21%), Space: 162 MB (5.21%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
@@ -1,61 +1,45 @@
 class Solution {
 public:
-    const int m=1e9+7;
     int maxSumMinProduct(vector<int>& nums) 
     {
         int n=nums.size();
-        stack<int>s;
-        vector<int>left(n,0),right(n,0);
+        vector<int>left(n,-1);
+        vector<int>right(n,n);
+        vector<long long>prefix(n,0);
+        vector<pair<int,int>>v;
         for(int i=0;i<n;i++)
         {
-            while(!s.empty()&&nums[s.top()]>=nums[i])
+            v.push_back({nums[i],i});
+            if(i==0)
             {
-                s.pop();
+                prefix[i]=nums[i];
+                continue;
             }
-            if(s.size()==0)
-            {
-                left[i]=-1;
-            }
-            else
-            {
-                left[i]=s.top();
-            }
-            s.push(i);
+            prefix[i]=prefix[i-1]+nums[i];
         }
-        while(s.size())
+        sort(v.begin(),v.end());
+        set<int>s;
+        for(auto &[val,i]:v)
         {
-            s.pop();
+            auto it=s.lower_bound(i);
+            if(it!=s.end())
+                right[i]=*it;
+                if(it!=s.begin())
+                {
+                    it--;
+                    left[i]=*it;
+                }
+            s.insert(i);
         }
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!s.empty()&&nums[s.top()]>=nums[i])
-            {
-                s.pop();
-            }
-            if(s.size()==0)
-            {
-                right[i]=n;
-            }
-            else
-            {
-                right[i]=s.top();
-            }
-            s.push(i);
-        }
-        vector<long long>sum(n+1,0);
-        for(int i=1;i<=n;i++)
-        {
-            sum[i]=sum[i-1]+nums[i-1];
-        }
+        int mod=1e9+7;
         long long ans=0;
         for(int i=0;i<n;i++)
         {
             int l=left[i];
             int r=right[i];
-            long long x1=(sum[r]-sum[l+1]);
-            long long val=nums[i]*x1;
+            long long val=1ll*nums[i]*(prefix[r-1]-((l!=-1)?prefix[l]:0));
             ans=max(ans,val);
         }
-        return ans%m;
+        return ans%mod;
     }
 };
