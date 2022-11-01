129e118fc043359e60d1c2108d8885be15fdea86
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-11-01 11:40:44 +0530

    Time: 1675 ms (26.21%), Space: 305.2 MB (18.77%) - LeetHub

---
+++ 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int dp[4010];
+    vector<int>kmp(string s)
+    {
+        vector<int>lps(s.size(),0);
+        int i=1;
+        int len=0;
+        while(i<s.size())
+        {
+            if(s[i]==s[len])
+            {
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else
+            {
+                if(len)
+                {
+                    len=lps[len-1];
+                }
+                else
+                {
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+    int find(vector<vector<int>>&nums,int index)
+    {
+        if(index>=nums.size())
+        {
+            return 0;
+        }
+        if(dp[index]!=-1)
+        {
+            return dp[index];
+        }
+        int ans=1;
+        for(int i=1;i<nums[index].size();i++)
+        {
+            if((i+1)==nums[index][i]*2)
+            {
+                ans=max(ans,1+find(nums,nums[index][i]+index));
+            }
+        }
+        return dp[index]=ans;
+    }
+    int deleteString(string s) 
+    {
+        memset(dp,-1,sizeof(dp));
+        vector<vector<int>>nums(s.size());
+        for(int i=0;i<s.size();i++)
+        {
+            nums[i]=kmp(s.substr(i));
+        }
+        return find(nums,0);
+    }
+};
