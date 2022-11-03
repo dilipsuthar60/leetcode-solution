a5b559daac87d1bcd0654e122c43d352c2a76ce6
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-11-03 16:52:23 +0530

    Time: 1092 ms (22.95%), Space: 185 MB (10.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,31 +2,60 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words) 
     {
-        int dp[27][27];
-        memset(dp,0,sizeof(dp));
-        int len=0;
+        int count=0;
+        unordered_map<string,int>mp;
         for(auto it:words)
         {
-            int first=it[0]-'a';
-            int second=it[1]-'a';
-            if(dp[second][first])
+            string s=it;
+            reverse(s.begin(),s.end());
+            if(mp.find(s)!=mp.end())
             {
-                len+=4;
-                dp[second][first]--;
+                count+=4;
+                mp[s]--;
+                if(mp[s]==0)
+                {
+                    mp.erase(s);
+                }
             }
             else
             {
-                dp[first][second]++;
+                mp[it]++;
             }
         }
-        for(int i=0;i<26;i++)
+        for(auto &[a,b]:mp)
         {
-            if(dp[i][i])
+            if(a[0]==a[1]&&b)
             {
-                len+=2;
+                count+=2;
                 break;
             }
         }
-        return len;
+        return count;
+        // int dp[27][27];
+        // memset(dp,0,sizeof(dp));
+        // int len=0;
+        // for(auto it:words)
+        // {
+        //     int first=it[0]-'a';
+        //     int second=it[1]-'a';
+        //     if(dp[second][first])
+        //     {
+        //         len+=4;
+        //         dp[second][first]--;
+        //     }
+        //     else
+        //     {
+        //         dp[first][second]++;
+        //     }
+        // }
+        // for(int i=0;i<26;i++)
+        // {
+        //     if(dp[i][i])
+        //     {
+        //         len+=2;
+        //         break;
+        //     }
+        // }
+        // return len;
     }
 };
