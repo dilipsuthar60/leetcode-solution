87c368765e9541425ccc8ee13bf415367c140c79
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-11-13 12:37:40 +0530

    Added solution - LeetHub

---
+++ Fill up buckets - GFG/fill-up-buckets.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int totalWays(int n, vector<int>capacity) {
+        // code here
+        long long ans=1;
+        int mod=1e9+7;
+        sort(capacity.begin(),capacity.end());
+        for(int i=0;i<n;i++)
+        {
+            ans*=(capacity[i]-i);
+            ans%=mod;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>capacity(n);
+		for(int i = 0; i < n; i++)
+			cin >> capacity[i];
+		Solution ob;
+		int ans = ob.totalWays(n, capacity);
+		cout << ans << "\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
