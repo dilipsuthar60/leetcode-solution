cdfb5b42e1015799f52d4839842537e1c88e6ac1
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-11-18 14:42:30 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (71.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int dp[10005];
-    int find(string&s,int index)
+    int numDecodings(string s) 
     {
-        if(index==s.size())
+        int n=s.size();
+        int dp[n+1];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        dp[1]=(s[0]=='0')?0:1;
+        for(int i=2;i<=n;i++)
         {
-            return 1;
-        }
-        if(dp[index]!=-1)
-        {
-            return dp[index];
-        }
-        if(s[index]=='0')
-        {
-            return 0;
-        }
-        int ans=find(s,index+1);
-        if(index+1<s.size())
-        {
-            int val=stoi(s.substr(index,2));
-            if(val>=10&&val<=26)
+            int val1=stoi(s.substr(i-1,1));
+            int val2=stoi(s.substr(i-2,2));
+            if(val1>=1&&val1<=9)
             {
-                ans+=find(s,index+2);
+                dp[i]+=dp[i-1];
+            }
+            if(val2>=10&&val2<=26)
+            {
+                dp[i]+=dp[i-2];
             }
         }
-        return dp[index]=ans;
-    }
-    int numDecodings(string s) {
-        memset(dp,-1,sizeof(dp));
-        return find(s,0);
+        return dp[n];
     }
 };
