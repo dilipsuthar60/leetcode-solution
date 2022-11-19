284dde0553415d3aefcbbcdefc46eb9883791187
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-11-19 09:41:01 +0530

    Added solution - LeetHub

---
+++ Check if all levels of two trees are anagrams or not - GFG/check-if-all-levels-of-two-trees-are-anagrams-or-not.cpp
@@ -0,0 +1,137 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    void find(Node*root,map<int,vector<int>>&dp,int level=0)
+    {
+        if(root==NULL)
+        {
+            return ;
+        }
+        dp[level].push_back(root->data);
+        find(root->left,dp,level+1);
+        find(root->right,dp,level+1);
+    }
+    bool check(vector<int>&v1,vector<int>&v2)
+    {
+        sort(v1.begin(),v1.end());
+        sort(v2.begin(),v2.end());
+        return v1==v2;
+    }
+    bool areAnagrams(Node *root1, Node *root2)
+    {
+        map<int,vector<int>>dp1,dp2;
+        find(root1,dp1);
+        find(root2,dp2);
+        int size=dp1.size();
+        for(int i=0;i<size;i++)
+        {
+            auto first=dp1[i];
+            auto second=dp2[i];
+            if(!check(first,second))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+int main() {
+    int t;
+    scanf("%d\n", &t);
+    while (t--) {
+        string treeString1, treeString2;
+        getline(cin, treeString1);
+        Node *root1 = buildTree(treeString1);
+        getline(cin, treeString2);
+        Node *root2 = buildTree(treeString2);
+        Solution ob;
+        cout << ob.areAnagrams(root1, root2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
