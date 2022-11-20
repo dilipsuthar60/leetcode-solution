1e6de43e54bca307ebe69a4a2c9d2c107f9edc16
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-11-20 22:29:56 +0530

    Time: 159 ms (14.46%), Space: 10.7 MB (43.49%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -1,49 +1,20 @@
 class Solution {
 public:
-    long long find(string &s,int k)
-    {
-        long long  count=0;
-        long long dp[26];
-        memset(dp,0,sizeof(dp));
-        int n=s.size();
-        int unique=0;
-        for(int i=0,j=0;i<n;i++)
-        {
-            if(dp[s[i]-'a']==0)
-            {
-                unique++;
-            }
-             dp[s[i]-'a']++;
-            while(unique>k)
-            {
-                 dp[s[j]-'a']--;
-                if(dp[s[j]-'a']==0)
-                {
-                    unique--;
-                }
-                j++;
-            }
-            count+=(i-j+1);
-        }
-        return count;
-    }
     long long appealSum(string s) 
     {
         int n=s.size();
-        long long count=0;
-        vector<int>dp(26,-1);
+        unordered_map<char,int>dp;
+        long long ans=0;
         for(int i=0;i<n;i++)
         {
-            long long sum=(n-i)*(i-dp[s[i]-'a']);
-            count+=sum;
-            dp[s[i]-'a']=i;
+            int val=-1;
+            if(dp.count(s[i]))
+            {
+                val=dp[s[i]];
+            }
+            ans+=1ll*(n-i)*(i-val);
+            dp[s[i]]=i;
         }
-        return count;
-        // for(int i=1;i<=26;i++)
-        // {
-        //     long long x=find(s,i)-find(s,i-1);
-        //     count+=(1ll*x*i);
-        // }
-        // return count;
+        return ans;
     }
 };
