8860605c47cd7cd1e4ede82ca8335b8becb3eca8
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-11-20 16:59:48 +0530

    Time: 1882 ms (16.67%), Space: 202.3 MB (16.67%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int>dp[100005];
+    void find(int node,vector<int>&sub,int p=-1)
+    {
+        for(auto it:dp[node])
+        {
+            if(it!=p)
+            {
+                find(it,sub,node);
+                sub[node]+=sub[it];
+            }
+        }
+    }
+    long long minimumFuelCost(vector<vector<int>>&nums, int seats) 
+    {
+        for(auto it:nums)
+        {
+            dp[it[0]].push_back(it[1]);
+            dp[it[1]].push_back(it[0]);
+        }
+        int n=nums.size();
+        vector<int>sub(n+1,1);
+        find(0,sub);
+        long long ans=0;
+        for(int i=1;i<=n;i++)
+        {
+            ans+=(sub[i]+seats-1)/seats;
+        }
+        return ans;
+    }
+};
