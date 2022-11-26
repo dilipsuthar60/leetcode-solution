d42d9da89be3cea6c69886ed7c7380c1fce86773
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-11-26 22:01:18 +0530

    Time: 1323 ms (8.33%), Space: 71.1 MB (16.67%) - LeetHub

--- 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
+++ 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
@@ -8,42 +8,42 @@ public:
         {
             return count==5;
         }
-        if(dp[index][first][second][count]!=-1)
+        if(dp[index][first+1][second+1][count]!=-1)
         {
-            return dp[index][first][second][count]%mod;
+            return dp[index][first+1][second+1][count]%mod;
         }
         long long ans=find(s,index+1,first,second,count);
         if(count==0)
         {
             ans+=find(s,index+1,s[index]-'0',second,count+1);
         }
-        else if(count==1)
+        if(count==1)
         {
             ans+=find(s,index+1,first,s[index]-'0',count+1);
         }
-        else if(count==2)
+        if(count==2)
         {
             ans+=find(s,index+1,first,second,count+1);
         }
-        else if(count==3)
+        if(count==3)
         {
             if((s[index]-'0')==second)
             {
                 ans+=find(s,index+1,first,second,count+1);
             }
         }
-        else if(count==4)
+        if(count==4)
         {
             if((s[index]-'0')==first)
             {
                 ans+=find(s,index+1,first,second,count+1);
             }
         }
-        return dp[index][first][second][count]= ans%mod;
+        return dp[index][first+1][second+1][count]= ans%mod;
         
     }
     int countPalindromes(string s) {
         memset(dp,-1,sizeof(dp));
-        return find(s,0,10,10,0)%mod;
+        return find(s,0,-1,-1,0)%mod;
     }
 };
