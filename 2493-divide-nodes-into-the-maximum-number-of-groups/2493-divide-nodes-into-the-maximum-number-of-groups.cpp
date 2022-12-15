2810e09a0270dbc61327ca96c573ea08816e80e7
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-12-15 18:05:20 +0530

    Time: 373 ms (90.38%), Space: 53 MB (32.83%) - LeetHub

---
+++ 2493-divide-nodes-into-the-maximum-number-of-groups/2493-divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -0,0 +1,115 @@
+class Solution {
+public:
+    vector<int>dp[510];
+    bool checkOddCycle(int node,vector<int>&vis)
+    {
+        queue<int>q;
+        q.push(node);
+        vis[node]=1;
+        vector<int>col(vis.size(),0);
+        col[node]=1;
+        while(q.size())
+        {
+            auto temp=q.front();
+            q.pop();
+            for(auto &it:dp[temp])
+            {
+                if(vis[it]==0)
+                {
+                    vis[it]=1;
+                    q.push(it);
+                    col[it]=(col[temp]^1);
+                }
+                else if(col[it]==col[temp])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    int findFarDistance(int node,int n)
+    {
+        queue<int>q;
+        q.push(node);
+        vector<int>vis(n+1,0);
+        vis[node]=1;
+        int level=0;
+        while(q.size())
+        {
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                auto temp=q.front();
+                q.pop();
+                for(auto it:dp[temp])
+                {
+                    if(vis[it]==0)
+                    {
+                        q.push(it);
+                        vis[it]=1;
+                    }
+                }
+            }
+            level++;
+        }
+        return level;
+    }
+    int findMaxOfAllNodeGroup(int node,int n,vector<int>&vis, vector<int>&dis)
+    {
+        queue<int>q;
+        q.push(node);
+        vis[node]=1;
+        int maxValue=0;
+        while(q.size())
+        {
+            auto temp=q.front();
+            q.pop();
+            maxValue=max(maxValue,dis[temp]);
+            for(auto it:dp[temp])
+            {
+                if(vis[it]==0)
+                {
+                    q.push(it);
+                    vis[it]=1;
+                }
+            }
+        }
+        return maxValue;
+        
+    }
+    int magnificentSets(int n, vector<vector<int>>&edges) 
+    {
+        for(auto &it:edges)
+        {
+            dp[it[0]].push_back(it[1]);
+            dp[it[1]].push_back(it[0]);
+        }
+        vector<int>vis(n+1,0);
+        for(int i=1;i<=n;i++)
+        {
+            if(vis[i]==0)
+            {
+                if(!checkOddCycle(i,vis))
+                {
+                    return -1;
+                }
+            }
+        }
+        vector<int>dis(n+1,0);
+        for(int i=1;i<=n;i++)
+        {
+            dis[i]=findFarDistance(i,n);
+        }
+        vis=vector<int>(n+1,0);
+        int group=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(vis[i]==0)
+            {
+                group+=findMaxOfAllNodeGroup(i,n,vis,dis);
+            }
+        }
+        return group;
+    }
+};
