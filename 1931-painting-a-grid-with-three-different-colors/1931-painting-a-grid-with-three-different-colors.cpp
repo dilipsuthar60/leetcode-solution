4963f76844c08df327f05d3c01922ffe6dd8eea4
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-12-26 19:09:19 +0530

    Time: 791 ms (26.47%), Space: 7.8 MB (86.76%) - LeetHub

--- 1931-painting-a-grid-with-three-different-colors/1931-painting-a-grid-with-three-different-colors.cpp
+++ 1931-painting-a-grid-with-three-different-colors/1931-painting-a-grid-with-three-different-colors.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int mod=1e9+7;
-    unordered_map<string,long long>dp;
+    long long dp[1001][100];
     vector<vector<int>>result;
     void allColor(vector<int>&nums,int index,int m,vector<int>curr)
     {
@@ -20,56 +20,93 @@ public:
             }
         }
     }
-    string get(vector<int>&v)
+    // string get(vector<int>&v)
+    // {
+    //     string s;
+    //     for(int i=0;i<v.size();i++)
+    //     {
+    //         s.push_back(char(v[i]+'0'));
+    //     }
+    //     return s;
+    // }
+    // bool check(string &p,string &s)
+    // {
+    //     if(p.size()==0)
+    //     {
+    //         return true;
+    //     }
+    //     for(int i=0;i<p.size();i++)
+    //     {
+    //         if(p[i]==s[i])
+    //         {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // long long find(int n,int index,string prev)
+    // {
+    //     if(n==index)
+    //     {
+    //         return 1;
+    //     }
+    //     string hash=to_string(index)+"#"+prev;
+    //     if(dp.find(hash)!=dp.end())
+    //     {
+    //         return dp[hash]%mod;
+    //     }
+    //     long long ans=0;
+    //     for(auto &it:result)
+    //     {
+    //         string curr=get(it);
+    //         if(check(prev,curr))
+    //         {
+    //             ans+=find(n,index+1,curr);
+    //         }
+    //     }
+    //     return dp[hash] =ans%mod;
+    // }
+    long long find(vector<vector<int>>&result,int curr,int index,int &n)
     {
-        string s;
-        for(int i=0;i<v.size();i++)
-        {
-            s.push_back(char(v[i]+'0'));
-        }
-        return s;
-    }
-    bool check(string &p,string &s)
-    {
-        if(p.size()==0)
-        {
-            return true;
-        }
-        for(int i=0;i<p.size();i++)
-        {
-            if(p[i]==s[i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-    long long find(int n,int index,string prev)
-    {
-        if(n==index)
+        if(index==n)
         {
             return 1;
         }
-        string hash=to_string(index)+"#"+prev;
-        if(dp.find(hash)!=dp.end())
+        if(dp[index][curr]!=-1)
         {
-            return dp[hash]%mod;
+            return dp[index][curr]%mod;
         }
         long long ans=0;
-        for(auto &it:result)
+        for(int i=0;i<result.size();i++)
         {
-            string curr=get(it);
-            if(check(prev,curr))
+            int yes=1;
+            for(int j=0;j<result[0].size();j++)
             {
-                ans+=find(n,index+1,curr);
+                if(result[curr][j]==result[i][j])
+                {
+                    yes=0;
+                    break ;
+                }
+            }
+            if(yes)
+            {
+
+                ans+=find(result,i,index+1,n);
             }
         }
-        return dp[hash] =ans%mod;
+        return dp[index][curr]=ans%mod;
     }
     int colorTheGrid(int m, int n) {
+        memset(dp,-1,sizeof(dp));
         vector<int>nums={1,2,3};
         vector<int>curr;
         allColor(nums,0,m,curr);
-        return (int)find(n,0,"")%mod;
+        long long ans=0;
+        for(int i=0;i<result.size();i++)
+        {
+            ans+=find(result,i,1,n);
+            ans%=mod;
+        }
+        return ans;
     }
 };
