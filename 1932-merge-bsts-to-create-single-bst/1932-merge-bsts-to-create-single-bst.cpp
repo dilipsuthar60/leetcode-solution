d8a9c7937492682398efc1185f70db20dc30ddd4
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-12-27 20:16:28 +0530

    Time: 812 ms (85.71%), Space: 429.4 MB (56.25%) - LeetHub

---
+++ 1932-merge-bsts-to-create-single-bst/1932-merge-bsts-to-create-single-bst.cpp
@@ -0,0 +1,91 @@
+class Solution 
+{
+public:
+    bool check_bst(TreeNode*root,int l=-1e9,int r=1e9)
+    {
+        if(root==NULL)
+        {
+            return true;
+        }
+        if(!(root->val>l&&root->val<r))
+        {
+            return false;
+        }
+        return check_bst(root->left,l,root->val)&&check_bst(root->right,root->val,r);
+    }
+    void find(TreeNode*&root,unordered_map<int,TreeNode*>&mp)
+    {
+        if(root==NULL)
+        {
+            return ;
+        }
+        if(root->left)
+        {
+            if(mp[root->left->val])
+            {
+                root->left=mp[root->left->val];
+                mp[root->left->val]=NULL;
+            }
+        }
+        if(root->right)
+        {
+            if(mp[root->right->val])
+            {
+                root->right=mp[root->right->val];
+                mp[root->right->val]=NULL;
+            }
+        }
+        find(root->left,mp);
+        find(root->right,mp);
+    }
+    TreeNode* canMerge(vector<TreeNode*>& trees) 
+    {
+        unordered_map<int,int>mp;
+        for(auto it:trees)
+        {
+            mp[it->val]++;
+            if(it->left!=NULL)
+            {
+                mp[it->left->val]++;
+            }
+            if(it->right!=NULL)
+            {
+                mp[it->right->val]++;
+            }
+        }
+        TreeNode*root=NULL;
+        for(auto it:trees)
+        {
+            if(mp[it->val]==1)
+            {
+                if(root==NULL)
+                {
+                    root=it;
+                }
+                else
+                {
+                    return NULL;
+                }
+            }
+        }
+        unordered_map<int,TreeNode*>node;
+        for(auto it:trees)
+        {
+            node[it->val]=it;
+        }
+        find(root,node);
+        int count=0;
+        for(auto it:node)
+        {
+            if(it.second!=NULL)
+            {
+                count++;
+            }
+        }
+        if((!check_bst(root))||count>1)
+        {
+            return NULL;
+        }
+        return root;
+    }
+};
