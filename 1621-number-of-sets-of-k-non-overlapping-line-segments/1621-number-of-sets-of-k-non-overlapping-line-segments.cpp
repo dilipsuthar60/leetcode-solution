964ef13ae5f6fe20d39f4676653bee22d35d142e
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-12-29 16:10:58 +0530

    Time: 189 ms (58.57%), Space: 23.7 MB (38.57%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,34 +1,43 @@
 class Solution {
 public:
     int mod=1e9+7;
-    long long dp[1001][1001][2];
-    long long find(int n,int k,int start)
+    long long storeOverlappingSubProblem[1001][1001][2];
+    long long calculateAllLines(int n,int k,int start)
     {
+        // we are find our answer
         if(k==0)
         {
             return 1;
         }
+        // negative case
         if(n<=0)
         {
             return 0;
         }
-        if(dp[n][k][start]!=-1)
+        // if this problem all ready computated then return this value
+        if(storeOverlappingSubProblem[n][k][start]!=-1)
         {
-            return dp[n][k][start]%mod;
+            return storeOverlappingSubProblem[n][k][start]%mod;
         }
-        long long ans=find(n-1,k,start);
+        // not including this point
+        long long ans=calculateAllLines(n-1,k,start);
         if(start)
         {
-            ans+=find(n-1,k,0);
+            // starting of the point
+            ans+=calculateAllLines(n-1,k,0);
         }
         else if(start==0)
         {
-            ans+=find(n,k-1,1);
+            // ending of the point
+            ans+=calculateAllLines(n,k-1,1);
         }
-        return dp[n][k][start]= ans%mod;
+        return storeOverlappingSubProblem[n][k][start]= ans%mod;
     }
     int numberOfSets(int n, int k) {
-        memset(dp,-1,sizeof(dp));
-        return (int)find(n,k,1)%mod;
+        // initialize our matrix(storeOverLappingSubProblem) to -1
+        
+        memset(storeOverlappingSubProblem,-1,sizeof(storeOverlappingSubProblem));
+        
+        return (int)calculateAllLines(n,k,1)%mod;
     }
 };
