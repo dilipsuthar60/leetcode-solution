26c7df696af7326c9be1a1d2b350beefcfce678e
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-12-30 16:09:11 +0530

    Time: 707 ms (67.30%), Space: 196.1 MB (49.79%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>>dp;
     int ans=0;
+    vector<int>dp[100004];
     int find(int node,string &s,int p=-1)
     {
         int max1=0;
         int max2=0;
         for(auto &it:dp[node])
         {
-            if(it==p)
+            if(p!=it)
             {
-                continue;
-            }
-            int len=find(it,s,node);
-            if(s[it]!=s[node])
-            {
-                if(len>=max1)
+                int len=find(it,s,node);
+                if(s[it]!=s[node])
                 {
-                    max2=max1;
-                    max1=len;
-                }
-                else if(len>=max2)
-                {
-                    max2=len;
+                    if(len>=max1)
+                    {
+                        max2=max1;
+                        max1=len;
+                    }
+                    else if(len>=max2)
+                    {
+                        max2=len;
+                    }
                 }
             }
         }
@@ -32,13 +30,15 @@ public:
     }
     int longestPath(vector<int>& parent, string s) 
     {
-        n=parent.size();
-        dp=vector<vector<int>>(n+1);
-        for(int i=1;i<n;i++)
+        for(int i=0;i<parent.size();i++)
         {
-            dp[parent[i]].push_back(i);
-            dp[i].push_back(parent[i]);
+            if(parent[i]!=-1)
+            {
+                dp[parent[i]].push_back(i);
+                dp[i].push_back(parent[i]);
+            }
         }
+        ans=0;
         find(0,s);
         return ans;
     }
