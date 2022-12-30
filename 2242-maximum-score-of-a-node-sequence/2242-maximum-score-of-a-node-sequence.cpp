0f43c64396d0e4293668e9a57aa3e19af37a8c3f
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2022-12-30 23:12:39 +0530

    Time: 558 ms (88.51%), Space: 162.5 MB (47.97%) - LeetHub

--- 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.cpp
+++ 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    int maximumScore(vector<int>& sc, vector<vector<int>>&nums)
-    {    
-        int n=sc.size();
+    int maximumScore(vector<int>& nums, vector<vector<int>>& edges) 
+    {
+        int n=nums.size();
         set<pair<int,int>>dp[n+1];
-        for(auto &it:nums)
+        for(auto &it:edges)
         {
-            dp[it[0]].insert({sc[it[1]],it[1]});
-            dp[it[1]].insert({sc[it[0]],it[0]});
-            if(dp[it[0]].size()>3)
+            int a=it[0];
+            int b=it[1];
+            dp[a].insert({nums[b],b});
+            dp[b].insert({nums[a],a});
+            if(dp[a].size()>3)
             {
-                dp[it[0]].erase(dp[it[0]].begin());
+                dp[a].erase(dp[a].begin());
             }
-            if(dp[it[1]].size()>3)
+            if(dp[b].size()>3)
             {
-                dp[it[1]].erase(dp[it[1]].begin());
+                dp[b].erase(dp[b].begin());
             }
         }
+        auto check=[&](int a,int b,int c,int d)
+        {
+            return (a!=c)&&(a!=d)&&(b!=c)&&(b!=d)&&(c!=d);
+        };
         int ans=-1;
-        for(auto &it:nums)
+        for(auto &it:edges)
         {
             int a=it[0];
             int b=it[1];
@@ -26,9 +32,9 @@ public:
             {
                 for(auto &t2:dp[b])
                 {
-                    if(a!=t1.second&&b!=t1.second&&t2.second!=a&&t2.second!=b&&a!=b&&t1.second!=t2.second)
+                    if(check(a,b,t1.second,t2.second))
                     {
-                        ans=max(ans,sc[a]+sc[b]+t1.first+t2.first);
+                        ans=max(ans,nums[a]+nums[b]+t1.first+t2.first);
                     }
                 }
             }
