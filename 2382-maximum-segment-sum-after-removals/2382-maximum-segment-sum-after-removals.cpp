da43de41abcfaa34309c1a9fbc6f16a0dd6cebff
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-01-06 19:31:41 +0530

    Time: 346 ms (80.70%), Space: 110.9 MB (66.08%) - LeetHub

--- 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.cpp
+++ 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.cpp
@@ -1,54 +1,71 @@
 class Solution {
 public:
-    int find(int x,vector<long long>&parent)
+    int n;
+    long long max_val=0;
+    vector<long long>parent,sum,rank;
+    int find(int x)
     {
         if(x==parent[x])
         {
             return x;
         }
-        return parent[x]=find(parent[x],parent);
+        return parent[x]=find(parent[x]);
     }
-    vector<long long> maximumSegmentSum(vector<int>& nums, vector<int>& q) 
+    void merge(int x,int y)
     {
-        int n=nums.size();
-        vector<long long>parent(n),sum(n),vis(n,0);
-        iota(parent.begin(),parent.end(),0);
+        x=find(x);
+        y=find(y);
+        if(x==y)
+        {
+            return ;
+        }
+        // max_val=max(max_val,sum[y]);
+        // parent[x]=y;
+        if(rank[x]<rank[y])
+        {
+            rank[y]+=rank[x];
+            parent[x]=y;
+            sum[y]+=sum[x];
+            max_val=max(max_val,sum[y]);
+        }
+        else
+        {
+            rank[x]+=rank[y];
+            parent[y]=x;
+            sum[x]+=sum[y];
+            max_val=max(max_val,sum[x]);
+        }
+    }
+    vector<long long> maximumSegmentSum(vector<int>& nums, vector<int>& r)
+    {
+        n=nums.size();
+        parent.resize(n+10);
+        sum.resize(n+10);
+        rank.resize(n+10);
         for(int i=0;i<n;i++)
         {
+            rank[i]=1;
             sum[i]=nums[i];
+            parent[i]=i;
         }
-        vector<long long>ans;
-        long long max_val=0;
+        vector<int>vis(n+1,0);
+        vector<long long>ans(n,0);
+         max_val=0;
         for(int i=n-1;i>=0;i--)
         {
-            ans.push_back(max_val);
-            int index=q[i];
+            ans[i]=max_val;
+            int index=r[i];
             vis[index]=1;
             max_val=max(max_val,1ll*nums[index]);
-            if(index-1>=0&&vis[index-1])
-            {
-                int first=find(index,parent);
-                int second=find(index-1,parent);
-                if(first!=second)
-                {
-                    sum[first]+=sum[second];
-                    parent[second]=first;
-                    max_val=max(max_val,sum[first]);
-                }
-            }
             if(index+1<n&&vis[index+1])
             {
-                int first=find(index,parent);
-                int second=find(index+1,parent);
-                if(first!=second)
-                {
-                    sum[first]+=sum[second];
-                    parent[second]=first;
-                    max_val=max(max_val,sum[first]);
-                }
+                merge(index,index+1);
+            }
+            if(index-1>=0&&vis[index-1])
+            {
+                merge(index,index-1);
             }
         }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
