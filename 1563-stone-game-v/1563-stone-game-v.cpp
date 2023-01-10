b02deb28625e2afc8e471815dfa62c29e6a6a525
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-01-10 19:09:15 +0530

    Time: 1539 ms (40.50%), Space: 23.5 MB (29.00%) - LeetHub

---
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int find(vector<int>&nums,int l,int r)
+    {
+        if(l>=r)
+        {
+            return 0;
+        }
+        int ans=0;
+        if(dp[l][r]!=-1)
+        {
+            return dp[l][r];
+        }
+        for(int p=l;p<r;p++)
+        {
+            int left_sum=nums[p]-((l==0)?0:nums[l-1]);
+            int right_sum=nums[r]-nums[p];
+            if(left_sum<right_sum)
+            {
+                ans=max(ans,left_sum+find(nums,l,p));
+            }
+            else if(left_sum>right_sum)
+            {
+                ans=max(ans,right_sum+find(nums,p+1,r));
+            }
+            else
+            {
+                ans=max({ans,left_sum+find(nums,l,p),right_sum+find(nums,p+1,r)});
+            }
+        }
+        return dp[l][r] = ans;
+    }
+    int stoneGameV(vector<int>&nums) 
+    {
+        int n=nums.size();
+        dp=vector<vector<int>>(n+1,vector<int>(n+1,-1));
+        for(int i=1;i<n;i++)
+        {
+            nums[i]+=nums[i-1];
+        }
+        return find(nums,0,n-1);
+    }
+};
