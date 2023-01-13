cbb7ba8fb3658f59db58587f6f06a8b1efbd4a7b
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-01-13 18:53:54 +0530

    Time: 323 ms (82.76%), Space: 184.6 MB (48.85%) - LeetHub

---
+++ 1994-the-number-of-good-subsets/1994-the-number-of-good-subsets.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    vector<int>prime;
+    int mp[45];
+    long long dp[31][1<<12];
+    int find(int n)
+    {
+        for(int i=2;i<=sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    long long find(int value,int mask)
+    {
+        if(value>30)
+        {
+            return (mask>0);
+        }
+        if(dp[value][mask]!=-1)
+        {
+            return dp[value][mask]%mod;
+        }
+        int new_mask=0;
+        long long product=1;
+        for(int i=0;i<prime.size();i++)
+        {
+            if((value%prime[i])==0)
+            {
+                product*=prime[i];
+                new_mask|=(1<<i);
+            }
+        }
+        long long ans=find(value+1,mask);
+        if(product==value&&mp[value]&&((mask&new_mask)==0))
+        {
+            ans+=(find(value+1,mask|new_mask)*mp[value]);
+        }
+        return dp[value][mask]=ans%mod;
+    }
+    int numberOfGoodSubsets(vector<int>& nums) 
+    {
+        memset(dp,-1,sizeof(dp));
+        for(int i=2;i<=30;i++)
+        {
+            if(find(i))
+            {
+                prime.push_back(i);
+            }
+        }
+        for(auto it:nums)
+        {
+            mp[it]++;
+        }
+        long long val=find(2,0)%mod;
+        long one=1;
+        for(int i=0;i<mp[1];i++)
+        {
+            one=(one*2)%mod;
+        }
+        val=(val*one)%mod;
+        return val;
+    }
+};
