f73416cf859cbe49db6e11afc6deb15c4bd26ddc
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-01-14 23:04:35 +0530

    Time: 228 ms (64.44%), Space: 19.6 MB (40.00%) - LeetHub

--- 1883-minimum-skips-to-arrive-at-meeting-on-time/1883-minimum-skips-to-arrive-at-meeting-on-time.cpp
+++ 1883-minimum-skips-to-arrive-at-meeting-on-time/1883-minimum-skips-to-arrive-at-meeting-on-time.cpp
@@ -5,11 +5,7 @@ public:
     long long dp[1100][1100];
     long long find(vector<int>&nums,int index,int k)
     {
-          if(k<0)
-        {
-            return 1e9;
-        }
-        if(index<0)
+        if(index>=n)
         {
             return 0;
         }
@@ -17,8 +13,12 @@ public:
         {
             return dp[index][k];
         }
-        long long skip=1ll*nums[index]+find(nums,index-1,k-1);
-        long long not_skip=((1ll*nums[index]+speed-1+find(nums,index-1,k))/speed)*speed;
+        long long skip=1e18;
+        if(k){
+           skip=1ll*nums[index]+find(nums,index+1,k-1);
+        }
+        long long not_skip=1e18;
+         not_skip=((1ll*nums[index]+speed-1+find(nums,index+1,k))/speed)*speed;
         return dp[index][k]= min(skip,not_skip);
     }
     int minSkips(vector<int>&nums, int s, int hoursBefore) 
@@ -26,10 +26,10 @@ public:
         memset(dp,-1,sizeof(dp));
         speed=s;
         n=nums.size();
-        long long total=1ll*hoursBefore*speed-nums.back();
+        long long total=1ll*hoursBefore*speed;
         for(int i=0;i<n;i++)
         {
-            long long val=find(nums,n-2,i);
+            long long val=find(nums,0,i);
             if(val<=total)
             {
                 return i;
