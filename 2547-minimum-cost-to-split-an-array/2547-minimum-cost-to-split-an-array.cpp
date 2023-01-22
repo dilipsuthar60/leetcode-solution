70b958f7ac7362644e541c1483938ed92832e8fb
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-01-22 10:03:34 +0530

    Time: 754 ms (83.33%), Space: 133.1 MB (91.67%) - LeetHub

---
+++ 2547-minimum-cost-to-split-an-array/2547-minimum-cost-to-split-an-array.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int n;
+    long long memo[1004];
+    long long find(vector<int>&nums,int index,int &k,vector<vector<int>>&dp)
+    {
+        if(index>=n)
+        {
+            return 0ll;
+        }
+        if(memo[index]!=-1)
+        {
+            return memo[index];
+        }
+        long long ans=1e18;
+        for(int i=index;i<n;i++)
+        {
+            ans=min(ans,1ll*dp[index][i]+1ll*k+find(nums,i+1,k,dp));
+        }
+        return memo[index]= ans;
+    }
+    int minCost(vector<int>& nums, int k) {
+        n=nums.size();
+        memset(memo,-1,sizeof(memo));
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++)
+        {
+            int mp[1004];
+            int count=0;
+            memset(mp,0,sizeof(mp));
+            for(int j=i;j<n;j++)
+            {
+                mp[nums[j]]++;
+                if(mp[nums[j]]>=2)
+                {
+                    if(mp[nums[j]]==2)
+                    {
+                        count+=2;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+                dp[i][j]=count;
+            }
+        }
+        return find(nums,0,k,dp);
+    }
+};
