66697117b9ced06df9bbf842e8237008893eb41d
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-02-21 23:11:55 +0530

    Time: 395 ms (5.24%), Space: 77.9 MB (5.45%) - LeetHub

--- 2568-minimum-impossible-or/2568-minimum-impossible-or.cpp
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minImpossibleOR(vector<int>&arr) 
     {
-        unordered_set<int>s(arr.begin(),arr.end());
+    set<int>s(arr.begin(),arr.end());
         long long x=1;
         while(s.find(x)!=s.end())
         {
