d9f9bba561f77e39a0c192d9c9903bb2661f3be2
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-03-04 22:43:42 +0530

    Time: 802 ms (100.00%), Space: 222.7 MB (100.00%) - LeetHub

---
+++ 2581-count-number-of-possible-root-nodes/2581-count-number-of-possible-root-nodes.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int>dp[100005];
+    set<pair<int,int>>s;
+    int k;
+    int  find(int node,int p=-1)
+    {
+        int count=0;
+        for(auto it:dp[node])
+        {
+            if(it!=p)
+            {
+                count+=s.count({node,it})+find(it,node);
+            }
+        }
+        return count;
+    }
+    void find1(int node,int value,int &total,int p=-1)
+    {
+        if(value>=k)
+        {
+            total++;
+        }
+        for(auto it:dp[node])
+        {
+            if(it!=p)
+            {
+                find1(it,value-s.count({node,it})+s.count({it,node}),total,node);
+            }
+        }
+    }
+    int rootCount(vector<vector<int>>& edges, vector<vector<int>>& g, int K) 
+    {
+        k=K;
+        for(auto it:g)
+        {
+            s.insert({it[0],it[1]});
+        }
+        for(auto it:edges)
+        {
+            dp[it[0]].push_back(it[1]);
+            dp[it[1]].push_back(it[0]);
+        }
+        int total=0;
+        int value=find(0);
+        find1(0,value,total);
+        return total;
+    }
+};
