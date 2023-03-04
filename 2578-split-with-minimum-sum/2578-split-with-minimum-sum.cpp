f3bd3b3102916e2f307227105a4d7ade927cb3d8
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-03-04 23:07:54 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (66.67%) - LeetHub

--- 2578-split-with-minimum-sum/2578-split-with-minimum-sum.cpp
+++ 2578-split-with-minimum-sum/2578-split-with-minimum-sum.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    int it(string &s)
-    {
-        int sum=0;
-        for(int i=0;i<s.size();i++)
-        {
-            sum=sum*10+(s[i]-'0');
-        }
-        return sum;
-    }
-    int find(string &s)
-    {
-        int ans=1e9+188;
+    int splitNum(int num) {
+        string s=to_string(num);
+        sort(s.begin(),s.end());
+        int first=0;
+        int second=0;
         int n=s.size();
         for(int i=0;i<n;i++)
         {
-            string s1=s.substr(0,i+1);
-            string s2=s.substr(i+1);
-            ans=min(ans,it(s1)+it(s2));
+            if(i&1)
+            {
+                first=first*10+(s[i]-'0');
+            }
+            else
+            {
+                second=second*10+(s[i]-'0');
+            }
         }
-        return ans;
-    }
-    int splitNum(int num) 
-    {
-        int ans=1e9+100;
-        string s=to_string(num);
-        sort(s.begin(),s.end());
-        do{
-            ans=min(ans,find(s));
-        }while(next_permutation(s.begin(),s.end()));
-        return ans;
+        return first+second;
     }
 };
