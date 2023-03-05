e2c13a7a2e83edfc7773215eed98f748fc9cd7bf
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-03-05 09:35:31 +0530

    Time: 354 ms (100.00%), Space: 17.6 MB (33.33%) - LeetHub

---
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    long long dp[1050][1050];
+    long long find(vector<vector<int>>&nums,int index,int target)
+    {
+        if(index>=nums.size())
+        {
+            return target==0;
+        }
+        if(target<0)
+        {
+            return 0;
+        }
+        if(dp[index][target]!=-1)
+        {
+            return dp[index][target]%mod;
+        }
+        long long ans=find(nums,index+1,target);
+        for(int i=0;i<nums[index][0];i++)
+        {
+            if(target-(i+1)*nums[index][1]>=0)
+            {
+                ans+=find(nums,index+1,target-(i+1)*nums[index][1]);
+            }
+            else
+            {
+                 break;
+            }
+        }
+        return dp[index][target]= ans%mod;
+    }
+    int waysToReachTarget(int target, vector<vector<int>>& types) {
+        memset(dp,-1,sizeof(dp));
+        return find(types,0,target)%mod;
+    }
+};
