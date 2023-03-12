0bcf5d7c519af774dd9fc7527785b5ffddf92eb1
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-03-12 13:19:25 +0530

    Time: 105 ms (100.00%), Space: 39.8 MB (100.00%) - LeetHub

--- 2589-minimum-time-to-complete-all-tasks/2589-minimum-time-to-complete-all-tasks.cpp
+++ 2589-minimum-time-to-complete-all-tasks/2589-minimum-time-to-complete-all-tasks.cpp
@@ -3,7 +3,8 @@ public:
     int findMinimumTime(vector<vector<int>>&nums) 
     {
         sort(nums.begin(),nums.end(),[&](auto &a,auto &b){return a[1]<b[1];});
-        vector<int>vis(2001,0);
+        int vis[2001];
+        fill(vis,vis+2001,0);
         int count=0;
         for(auto it:nums)
         {
