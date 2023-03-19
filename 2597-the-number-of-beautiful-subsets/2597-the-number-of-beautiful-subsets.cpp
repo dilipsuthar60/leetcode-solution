298f31c1ca098f08612143b0e07064e0161336e4
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-03-19 14:07:46 +0530

    Time: 205 ms (100.00%), Space: 32.2 MB (23.08%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
-    int beautifulSubsets(vector<int>& nums, int k) 
+     int mp[2100];
+    int find(vector<int>&nums,int index,int k)
     {
-        sort(nums.begin(),nums.end());
-        int count=0;
-         int mp[2100]={0};
-        int n=nums.size();
-        for(int i=1;i<(1<<n);i++)
+        if(index>=nums.size())
         {
-            int f=1;
-            for(int j=0;j<n;j++)
-            {
-                if(i&(1<<j))
-                {
-                    if(mp[nums[j]-k+1000]==i)
-                    {
-                        f=0;
-                        break;
-                    }
-                    mp[nums[j]+1000]=i;
-                }
-            }
-            if(f)
-            {
-                count++;
-            }
+            return 1;
+        }
+        int count=find(nums,index+1,k);
+        if(mp[nums[index]-k+1000]==0)
+        {
+            mp[nums[index]+1000]++;
+            count+=find(nums,index+1,k);
+            mp[nums[index]+1000]--;
         }
         return count;
+    }
+    int beautifulSubsets(vector<int>& nums, int k) 
+    {
+        memset(mp,0,sizeof(mp));
+        sort(nums.begin(),nums.end());
+        return find(nums,0,k)-1;
+        // int count=0;
+        // int n=nums.size();
+        // for(int i=1;i<(1<<n);i++)
+        // {
+        //     int f=1;
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(i&(1<<j))
+        //         {
+        //             if(mp[nums[j]-k+1000]==i)
+        //             {
+        //                 f=0;
+        //                 break;
+        //             }
+        //             mp[nums[j]+1000]=i;
+        //         }
+        //     }
+        //     if(f)
+        //     {
+        //         count++;
+        //     }
+        // }
+        // return count;
     }
 };
