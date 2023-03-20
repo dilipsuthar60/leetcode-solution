71de114a4a064d391444efeaf3f308fda5f167fb
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-03-20 12:32:19 +0530

    Time: 209 ms (100.00%), Space: 32.2 MB (23.08%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -21,28 +21,5 @@ public:
         memset(mp,0,sizeof(mp));
         sort(nums.begin(),nums.end());
         return find(nums,0,k)-1;
-        // int count=0;
-        // int n=nums.size();
-        // for(int i=1;i<(1<<n);i++)
-        // {
-        //     int f=1;
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         if(i&(1<<j))
-        //         {
-        //             if(mp[nums[j]-k+1000]==i)
-        //             {
-        //                 f=0;
-        //                 break;
-        //             }
-        //             mp[nums[j]+1000]=i;
-        //         }
-        //     }
-        //     if(f)
-        //     {
-        //         count++;
-        //     }
-        // }
-        // return count;
     }
 };
