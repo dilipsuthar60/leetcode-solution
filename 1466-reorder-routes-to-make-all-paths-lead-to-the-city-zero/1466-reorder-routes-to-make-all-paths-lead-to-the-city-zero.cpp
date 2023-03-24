3339cfea777bd31a4fdeadf927c417edd35b6978
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-03-24 12:44:18 +0530

    Time: 407 ms (65.35%), Space: 105.9 MB (82.33%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,39 +1,39 @@
-class Solution {
-public:
-    int minReorder(int n, vector<vector<int>>& nums) 
+class Solution
+{
+    public: int minReorder(int n, vector<vector < int>> &nums)
     {
-        vector<int>dp1[n+10];
-        vector<int>dp2[n+10];
-        for(auto it:nums)
+        vector<int> dp1[n + 10];
+        vector<int> dp2[n + 10];
+        for (auto it: nums)
         {
-            int u=it[0];
-            int v=it[1];
+            int u = it[0];
+            int v = it[1];
             dp1[u].push_back(v);
             dp2[v].push_back(u);
         }
-        int cost=0;
-        queue<int>q;
+        int cost = 0;
+        queue<int> q;
         q.push(0);
-        vector<bool>vis(n+10,false);
-        while(q.size())
+        vector<bool> vis(n + 10, false);
+        while (q.size())
         {
-            auto temp=q.front();
+            auto temp = q.front();
             q.pop();
-            vis[temp]=true;
-            for(auto it:dp1[temp])
+            vis[temp] = true;
+            for (auto it: dp1[temp])
             {
-               if(vis[it]==false)
-               {
+                if (vis[it] == false)
+                {
                     q.push(it);
                     cost++;
-               }
+                }
             }
-             for(auto it:dp2[temp])
+            for (auto it: dp2[temp])
             {
-                 if(vis[it]==false)
-                 {
-                  q.push(it);   
-                 }
+                if (vis[it] == false)
+                {
+                    q.push(it);
+                }
             }
         }
         return cost;
