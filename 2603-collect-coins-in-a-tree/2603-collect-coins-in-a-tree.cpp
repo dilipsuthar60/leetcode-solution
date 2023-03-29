1e48af7ade3751126b7dd8041112defef06901a7
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-03-29 23:20:35 +0530

    Time: 2645 ms (5.09%), Space: 225 MB (63.70%) - LeetHub

---
+++ 2603-collect-coins-in-a-tree/2603-collect-coins-in-a-tree.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    vector<int>dp[31000];
+    int collectTheCoins(vector<int>& coins, vector<vector<int>>& edges) 
+    {
+        int n=coins.size();
+        vector<int>ind(n,0);
+        for(auto it:edges)
+        {
+            ind[it[0]]++;
+            ind[it[1]]++;
+            dp[it[0]].push_back(it[1]);
+            dp[it[1]].push_back(it[0]);
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            if(ind[i]==1&&coins[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        while(q.size())
+        {
+            auto temp=q.front();
+            q.pop();
+            for(auto it:dp[temp])
+            {
+                if(ind[it]>=1)
+                {
+                    ind[it]--;
+                    if(ind[it]==1&&coins[it]==0)
+                    {
+                        q.push(it);
+                    }
+                }
+            }
+            ind[temp]=0;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(ind[i]==1&&coins[i]==1)
+            {
+                q.push(i);
+            }
+        }
+        while(q.size())
+        {
+            auto temp=q.front();
+            q.pop();
+            for(auto it:dp[temp])
+            {
+                if(ind[it]>=1)
+                {
+                    ind[it]--;
+                    if(ind[it]==1)
+                    {
+                        ind[it]=0;
+                    }
+                }
+            }
+            ind[temp]=0;
+        }
+        int count=0;
+        for(auto it:edges)
+        {
+            if(ind[it[0]]&&ind[it[1]])
+            {
+                count++;
+            }
+        }
+        return 2*count;
+    }
+};
