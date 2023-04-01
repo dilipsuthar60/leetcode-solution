ca18e26bb993f7903bdc9920884634dcf549b14d
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-01 22:06:46 +0530

    Time: 3 ms (100.00%), Space: 21.8 MB (100.00%) - LeetHub

---
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minNumber(vector<int>& nums1, vector<int>& nums2) 
+    {
+        int dp1[10]={0};
+        int dp2[10]={0};
+        for(auto it:nums1)
+        {
+            dp1[it]++;
+        }
+        for(auto it:nums2)
+        {
+            dp1[it]++;
+        }
+        for(int i=1;i<=9;i++)
+        {
+            if(dp1[i]>=2)
+            {
+                return i;
+            }
+        }
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        return min(nums1[0]*10+nums2[0],nums2[0]*10+nums1[0]);
+    }
+};
