6d89fc74d71fcbc18d6bb789a90e50ad00a38b1d
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-01 22:27:39 +0530

    Time: 612 ms (100.00%), Space: 137.6 MB (100.00%) - LeetHub

--- 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.cpp
+++ 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.cpp
@@ -1,49 +1,38 @@
 class Solution {
 public:
-    vector<int> gr[5000];
-  int find(int n)
-{
-    int ans = INT_MAX;
-
-    for (int i = 0; i < n; i++) 
-    {
-        vector<int> dist(n, (int)(1e9));
- 
-        vector<int> par(n, -1);
- 
-        dist[i] = 0;
-        queue<int> q;
-        q.push(i);
-        while (!q.empty()) {
-
-            int x = q.front();
-            q.pop();
-            for (int child : gr[x]) {
-                if (dist[child] == (int)(1e9)) {
-                    dist[child] = 1 + dist[x];
-                    par[child] = x;
-                    q.push(child);
-                }
-
-                else if (par[x] != child and par[child] != x)
-                    ans = min(ans, dist[x] + dist[child] + 1);
-            }
-        }
-    }
- 
-    if (ans == INT_MAX)
-        return -1;
-    else
-        return ans;
-}
- 
-    int findShortestCycle(int N, vector<vector<int>>& edges) 
-    {
+    vector<vector<int>>dp;
+    int findShortestCycle(int n, vector<vector<int>>& edges) {
+        dp=vector<vector<int>>(n+1);
         for(auto it:edges)
         {
-            gr[it[0]].push_back(it[1]);
-            gr[it[1]].push_back(it[0]);
+            dp[it[0]].push_back(it[1]);
+            dp[it[1]].push_back(it[0]);
         }
-        return find(N);
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            vector<int>dis(n,-1);
+            queue<pair<int,int>>q;
+            q.push({i,-1});
+            dis[i]=0;
+            while(q.size())
+            {
+                auto [node,parent]=q.front();
+                q.pop();
+                for(auto it:dp[node])
+                {
+                    if(dis[it]==-1)
+                    {
+                        dis[it]=dis[node]+1;
+                        q.push({it,node});
+                    }
+                    else if(it!=parent)
+                    {
+                        ans=min(ans,dis[node]+dis[it]+1);
+                    }
+                }
+            }
+        }
+        return ans==INT_MAX?-1:ans;
     }
 };
