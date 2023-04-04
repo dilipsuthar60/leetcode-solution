03ebc1a92e039db6298be63ceba0fab2426eecff
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-04 17:48:31 +0530

    Time: 183 ms (84.41%), Space: 103.7 MB (92.10%) - LeetHub

--- 2611-mice-and-cheese/2611-mice-and-cheese.cpp
+++ 2611-mice-and-cheese/2611-mice-and-cheese.cpp
@@ -4,7 +4,7 @@ public:
     {
         int n=nums1.size();
         int total=accumulate(nums2.begin(),nums2.end(),0);
-        vector<int>diff(n);
+        vector<int>diff(n,0);
         for(int i=0;i<n;i++)
         {
             diff[i]=nums1[i]-nums2[i];
