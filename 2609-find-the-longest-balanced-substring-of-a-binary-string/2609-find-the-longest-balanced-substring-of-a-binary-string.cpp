6d78bb017d0da8bdb0b9b172bbfcf3e1b98ad4c0
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-04 17:50:44 +0530

    Time: 8 ms (45.87%), Space: 6.3 MB (25.25%) - LeetHub

--- 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.cpp
+++ 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    int find(string &s,int index)
-    {
-        int count0=0;
-        int count1=0;
-        int f=0;
-        for(int i=index;i<s.size();i++)
+    int findTheLongestBalancedSubstring(string s) {
+        int n=s.size();
+        int ans=0;
+        for(int i=0;i<n;)
         {
-            if(s[i]=='0'&&f==0)
+            int count0=0;
+            int count1=0;
+            while(i<n&&s[i]=='0')
             {
                 count0++;
+                i++;
             }
-            else if(s[i]=='1')
+            while(i<n&&s[i]=='1')
             {
-                f=1;
                 count1++;
+                i++;
             }
-            else
-            {
-                break;
-            }
-        }
-        return 2*min(count0,count1);
-    }
-    int findTheLongestBalancedSubstring(string s) {
-        int ans=0;
-        for(int i=0,n=s.size();i<n;i++)
-        {
-            if(s[i]=='0')
-            ans=max(ans,find(s,i));
+            ans=max(ans,2*min(count0,count1));
         }
         return ans;
     }
