d64fa66bb25a9a7d1c48d2a02e51723d7cd82df5
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-09 12:03:25 +0530

    Time: 274 ms (50.00%), Space: 109.6 MB (16.67%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void find(vector<int>&nums,vector<long long>&ans)
+    {
+        vector<int>temp(nums.begin(),nums.end());
+        long long right=accumulate(nums.begin(),nums.end(),0ll);
+        long long left=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            right-=nums[i];
+            ans[temp[i]]=(1ll*nums[i]*(i)-left)+(right-1ll*nums[i]*(n-i-1));
+            left+=nums[i];
+        }
+    }
+    vector<long long> distance(vector<int>& nums) 
+    {
+        int n=nums.size();
+        unordered_map<int,vector<int>>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]].push_back(i);
+        }
+        vector<long long>ans(n,0);
+        for(auto &[a,b]:mp)
+        {
+            find(b,ans);
+        }
+        return ans;
+    }
+};
