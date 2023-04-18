969cb75e0aefefd106811d4474735aa512e9e67c
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-18 22:15:43 +0530

    Time: 27 ms (54.31%), Space: 22.7 MB (9.69%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,48 +1,49 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode*merge(ListNode*l1,ListNode*l2)
+    ListNode*mergelist(ListNode*l1,ListNode*l2)
     {
-        if(l1==NULL||l2==NULL)
-        {
-            return l1==NULL?l2:l1;
-        }
-        ListNode*dummy_node=new ListNode(-1);
-        ListNode*head=dummy_node;
+        if(l1==NULL||l2==NULL) return l1==NULL?l2:l1;
+        ListNode*newHead=new ListNode(-1);
+        ListNode*newTail=newHead;
         while(l1!=NULL&&l2!=NULL)
         {
-            if((l1->val)<(l2->val))
+            if(l1->val<=l2->val)
             {
-                dummy_node->next=l1;
+                newTail->next=l1;
                 l1=l1->next;
             }
             else
             {
-                dummy_node->next=l2;
+                newTail->next=l2;
                 l2=l2->next;
             }
-            dummy_node=dummy_node->next;
+            newTail=newTail->next;
         }
-        dummy_node->next=(l1==NULL)?l2:l1;
-        return head->next;
+        newTail->next=(l1==NULL)?l2:l1;
+        return newHead->next;
     }
-    ListNode*find(vector<ListNode*>&nums,int l,int r)
+    ListNode*find(vector<ListNode*>&list,int l,int r)
     {
         if(l==r)
         {
-            return nums[l];
+            return list[l];
         }
         int mid=(l+r)/2;
-        ListNode*left=find(nums,l,mid);
-        ListNode*right=find(nums,mid+1,r);
-        return merge(left,right);
+        return mergelist(find(list,l,mid),find(list,mid+1,r));
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) 
     {
-        if(lists.size()==0)
-        {
-            return NULL;
-        }
+        if(lists.size()==0) return NULL;
         return find(lists,0,lists.size()-1);
     }
 };
