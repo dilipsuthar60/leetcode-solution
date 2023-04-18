bc1fecfe3a37f333de756259780342c3734b69dd
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-18 12:27:20 +0530

    Time: 4 ms (38.78%), Space: 6.2 MB (85.93%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -4,22 +4,34 @@ public:
         int n=s1.size();
         int m=s2.size();
         string ans(n+m,'a');
-        int i,j;
-        i=j=0;
-        int index=0;
-        while(i<n&&j<m)
+        for(int i=0,index=0;i<max(n,m);i++)
         {
-            ans[index++]=s1[i++];
-            ans[index++]=s2[j++];
+            if(i<n)
+            {
+                ans[index++]=s1[i];
+            }
+            if(i<m)
+            {
+                ans[index++]=s2[i];
+            }
         }
-        while(i<n)
-        {
-            ans[index++]=s1[i++];
-        }
-        while(j<m)
-        {
-            ans[index++]=s2[j++];
-        }
+        return ans;
+        // int i,j;
+        // i=j=0;
+        // int index=0;
+        // while(i<n&&j<m)
+        // {
+        //     ans[index++]=s1[i++];
+        //     ans[index++]=s2[j++];
+        // }
+        // while(i<n)
+        // {
+        //     ans[index++]=s1[i++];
+        // }
+        // while(j<m)
+        // {
+        //     ans[index++]=s2[j++];
+        // }
         return ans;
     }
 };
