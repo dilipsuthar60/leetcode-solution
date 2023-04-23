4b36f78b84ffb41736c156322191e0681be3da8b
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-23 13:39:47 +0530

    Time: 373 ms (60.00%), Space: 185.5 MB (10.00%) - LeetHub

--- 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.cpp
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.cpp
@@ -1,33 +1,60 @@
 class Solution {
 public:
-    int find(map<int,int>&mp,int x)
+    int bit[500];
+    void update(int i,int val)
     {
-        int count=0;
-        for(auto &[a,b]:mp)
+        i++;
+        while(i<500)
         {
-            count+=b;
-            if(count>=x)
+            bit[i]+=val;
+            i+=(i&-i);
+        }
+    }
+    int find(int i)
+    {
+        i++;
+        int sum=0;
+        while(i>0)
+        {
+            sum+=bit[i];
+            i-=(i&-i);
+        }
+        return sum;
+    }
+    int kth(int k)
+    {
+        int ans=0;
+        int l=0,r=100;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(find(mid)>k)
             {
-                return a>0?0:a;
+                ans=mid;
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
             }
         }
-        return -1;
+        ans=ans-50;
+        return ans>0?0:ans;
     }
     vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) 
     {
         int n=nums.size();
-        map<int,int>mp;
         for(int i=0;i<k;i++)
         {
-            mp[nums[i]]++;
+            update(nums[i]+50,1);
         }
         vector<int>v;
-        v.push_back(find(mp,x));
+        v.push_back(kth(x-1));
         for(int i=k;i<n;i++)
         {
-            mp[nums[i-k]]--;
-            mp[nums[i]]++;
-            v.push_back(find(mp,x));
+            update(nums[i-k]+50,-1);
+            update(nums[i]+50,1);
+            v.push_back(kth(x-1));
         }
         return v;
     }
