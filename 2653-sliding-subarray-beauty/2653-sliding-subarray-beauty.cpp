498c73e4f46245740c2d77fa1dd1d2d99989eb2a
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-23 13:17:36 +0530

    Time: 670 ms (10.00%), Space: 186.7 MB (10.00%) - LeetHub

--- 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.cpp
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.cpp
@@ -1,26 +1,33 @@
-
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <iostream>
-using namespace std;
-using namespace __gnu_pbds;
-template <class T> using multi_ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
 class Solution {
 public:
-    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
-        multi_ordered_set <int> s;
+    int find(map<int,int>&mp,int x)
+    {
+        int count=0;
+        for(auto &[a,b]:mp)
+        {
+            count+=b;
+            if(count>=x)
+            {
+                return a>0?0:a;
+            }
+        }
+        return -1;
+    }
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) 
+    {
+        int n=nums.size();
+        map<int,int>mp;
         for(int i=0;i<k;i++)
         {
-            s.insert(nums[i]);
+            mp[nums[i]]++;
         }
         vector<int>v;
-                    v.push_back(*s.find_by_order(x-1)>0?0:*s.find_by_order(x-1));
-
-        for(int i=k,n=nums.size();i<n;i++)
+        v.push_back(find(mp,x));
+        for(int i=k;i<n;i++)
         {
-            s.erase(s.find_by_order(s.order_of_key(nums[i-k])));
-            s.insert(nums[i]);
-            v.push_back(*s.find_by_order(x-1)>0?0:*s.find_by_order(x-1));
+            mp[nums[i-k]]--;
+            mp[nums[i]]++;
+            v.push_back(find(mp,x));
         }
         return v;
     }
