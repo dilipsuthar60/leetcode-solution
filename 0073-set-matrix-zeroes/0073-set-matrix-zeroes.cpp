9d49ede803848d9c3fcdf819187d163f43d4f644
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-04-28 17:40:17 +0530

    Time: 21 ms (14.10%), Space: 13.1 MB (92.86%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,27 +4,49 @@ public:
     {
         int n=mat.size();
         int m=mat[0].size();
-        vector<int>row(n);
-        vector<int>col(m);
+        int row_zero=false;
+        int col_zero=false;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(!mat[i][j])
                 {
-                    row[i]=1;
-                    col[j]=1;
+                    if(i==0)
+                    {
+                        row_zero=true;
+                    }
+                    if(j==0)
+                    {
+                        col_zero=true;
+                    }
+                    mat[i][0]=0;
+                    mat[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=1;j<m;j++)
             {
-                if(row[i]||col[j])
+                if(mat[i][0]==0||mat[0][j]==0)
                 {
                     mat[i][j]=0;
                 }
+            }
+        }
+        if(row_zero)
+        {
+            for(int i=0;i<m;i++)
+            {
+                mat[0][i]=0;
+            }
+        }
+        if(col_zero)
+        {
+            for(int i=0;i<n;i++)
+            {
+                mat[i][0]=0;
             }
         }
     }
