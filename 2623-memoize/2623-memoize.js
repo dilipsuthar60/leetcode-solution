14fe62f13c2a6323727aab6dbb91898db4d24b61
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-05-12 23:22:41 +0530

    Time: 347 ms (80.85%), Space: 109.7 MB (5.26%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    let memo={};
+    return function(...args) 
+    {
+        let key=args.join('#');
+        if(key in memo){
+            return memo[key];
+        }
+        return memo[key]=fn(...args);
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
