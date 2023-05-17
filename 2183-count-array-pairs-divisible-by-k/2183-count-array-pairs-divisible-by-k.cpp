a7fef3f44311e0fc9fd4d7e423e91fe4531e4d3f
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-05-17 21:10:45 +0530

    Time: 386 ms (38.14%), Space: 65 MB (70.23%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.cpp
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long countPairs(vector<int>& nums, int k) {
-        unordered_map<long long,long long>mp;
+        map<long long,long long>mp;
         int n=nums.size();
         long long ans=0;
         for(int i=0;i<n;i++)
