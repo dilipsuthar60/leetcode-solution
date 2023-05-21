ea2e05665eb146edd79b657b9d8ab633dd9b43da
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-05-21 12:21:26 +0530

    Time: 285 ms (53.78%), Space: 117.2 MB (63.70%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -4,30 +4,24 @@ public:
     {
         int n=mat.size();
         int m=mat[0].size();
-        vector<pair<int,int>>row,col;
+        vector<int>row(n,0),col(m,0);
         vector<vector<int>>ans(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
         {
-            int zero=count(mat[i].begin(),mat[i].end(),0);
-            int one=count(mat[i].begin(),mat[i].end(),1);
-            row.push_back({zero,one});
-        }
-        for(int j=0;j<m;j++)
-        {
-            int zero=0;
-            int one=0;
-            for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
             {
-                zero+=(mat[i][j]==0);
-                one+=(mat[i][j]==1);
+                if(!mat[i][j])
+                {
+                    row[i]++;
+                    col[j]++;
+                }
             }
-            col.push_back({zero,one});
         }
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                ans[i][j]=row[i].second+col[j].second-(row[i].first+col[j].first);
+                ans[i][j]=(m-row[i])+(n-col[j])-row[i]-col[j];
             }
         }
         return ans;
