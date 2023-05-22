656d40c1019561cda5b8e4fe00f352815e24b6a0
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-05-22 11:04:05 +0530

    Added solution - LeetHub

---
+++ Tree Transformation - GFG/tree-transformation.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    public:
+    int solve(int N, vector<int> p){
+        // code here
+         set<int> st;
+        for(int i=0;i<N;i++){
+            if(p[i]!=-1 && p[i]!=0){
+                st.insert(p[i]);
+            }
+        }
+        return st.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int N;
+        cin >> N;
+        vector<int> p(N);
+        for(int i = 0; i < N; i++){
+            cin >> p[i];
+        }
+
+        Solution obj;
+        cout << obj.solve(N, p) << "\n";
+    }
+}   
+// } Driver Code Ends
