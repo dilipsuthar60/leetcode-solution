ac0061c0de2c719dd272ca9eeb428ffc960a8ff7
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-05-27 22:14:18 +0530

    Time: 136 ms (90.91%), Space: 71 MB (81.82%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    unordered_set<string>st;
+    int dp[60];
+    int find(string &s,int index)
+    {
+        if(index>=s.size())
+        {
+            return 0;
+        }
+        if(dp[index]!=-1)
+        {
+            return dp[index];
+        }
+        int ans=0;
+        string curr="";
+        for(int i=index;i<s.size();i++)
+        {
+            curr.push_back(s[i]);
+            if(st.find(curr)!=st.end())
+            {
+            ans=max(ans,(int)curr.size()+find(s,i+1));
+            }
+        }
+        ans=max(ans,find(s,index+1));
+        return dp[index]= ans;
+    }
+    int minExtraChar(string s, vector<string>& dic) {
+        for(auto it:dic)
+        {
+            st.insert(it);
+        }
+        memset(dp,-1,sizeof(dp));
+        int val=find(s,0);
+        return s.size()-val;
+    }
+};
